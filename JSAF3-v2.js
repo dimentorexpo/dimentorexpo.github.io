53c907efb6587041597dd19f4fc952af8ca803a4
grumstv <grumstv@gmail.com>
2021-06-19 21:35:48 +0300

    Update JSAF3-v2.js

--- JSAF3-v2.js
+++ JSAF3-v2.js
@@ -65,7 +65,8 @@ var win_AFhelper =
 				<div style="margin: 5px;" id="1str">
 					<button id="languageAF" style="width:100px">Русский</button>
 					<button id="hideMenu" style="margin-left: 50px">hide</button>
-					<button id="setting" style="margin-left: 80px">S</button>
+					<button id="menu" style="margin-left: 100px">S1</button>
+					<button id="setting" style="margin-left: 80px">S1</button>
 				</div>
 				<div style="margin: 5px;" id="pages">
 				</div>
