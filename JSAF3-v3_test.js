f841bbe3b9460a75c6528483b8883ba9490f3588
grumstv <grumstv@gmail.com>
2021-07-09 18:38:15 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -2180,27 +2180,7 @@ async function getNotGoods(stringDate) {
 	goNotgood(list, list2, firstDate, secondDate)
 }
 
-var cstmTmp1 = document.createElement('div')
-		cstmTmp1.style = 'min-height: 25px; min-width: 65px; background: #464451; top: ' + localStorage.getItem('winCstmTmpsTop') + 'px; left: ' + localStorage.getItem('winCstmTmpsLeft') + 'px; font-size: 14px; z-index: 20; position: fixed; border: 1px solid rgb(56, 56, 56); color: black; border-radius:5px; border:1px solid #768d87; ';
-		cstmTmp1.id = 'cstmTmplates1'
-		cstmTmp1.style.display = 'none'
-		document.body.append(cstmTmp1);
-	} else {
-		cstmTmp = document.getElementById('cstmTmplates1')
-		while(document.getElementById('cstmTmplates1').children[0] != undefined)
-			document.getElementById('cstmTmplates1').children[0].remove()
-	}
-	
-	var buttonOpenTmpWindow1 = document.createElement('button')
-	buttonOpenTmpWindow1.innerHTML = 'L2'
-	buttonOpenTmpWindow1.style.marginLeft = '10px'
-	buttonOpenTmpWindow1.onclick = function() {
-		var a = document.getElementById('cstmTmplates1')
-		if(a.style.display == '')
-			a.style.display = 'none'
-		else
-			a.style.display = ''
-	}
+
 	
 
 function customTemplates(language = '') {
@@ -2234,6 +2214,29 @@ function customTemplates(language = '') {
 		else
 			a.style.display = ''
 	}
+	
+	var cstmTmp1 = document.createElement('div')
+		cstmTmp1.style = 'min-height: 25px; min-width: 65px; background: #464451; top: ' + localStorage.getItem('winCstmTmpsTop') + 'px; left: ' + localStorage.getItem('winCstmTmpsLeft') + 'px; font-size: 14px; z-index: 20; position: fixed; border: 1px solid rgb(56, 56, 56); color: black; border-radius:5px; border:1px solid #768d87; ';
+		cstmTmp1.id = 'cstmTmplates1'
+		cstmTmp1.style.display = 'none'
+		document.body.append(cstmTmp1);
+	} else {
+		cstmTmp = document.getElementById('cstmTmplates1')
+		while(document.getElementById('cstmTmplates1').children[0] != undefined)
+			document.getElementById('cstmTmplates1').children[0].remove()
+	}
+	
+	var buttonOpenTmpWindow1 = document.createElement('button')
+	buttonOpenTmpWindow1.innerHTML = 'L2'
+	buttonOpenTmpWindow1.style.marginLeft = '10px'
+	buttonOpenTmpWindow1.onclick = function() {
+		var a = document.getElementById('cstmTmplates1')
+		if(a.style.display == '')
+			a.style.display = 'none'
+		else
+			a.style.display = ''
+	}
+	
 	var tmpA = document.getElementById('AF_helper').children[0].children[0].children[0].children[0]
 	if(tmpA.children[1].innerHTML != 'tmps')
 		tmpA.insertBefore(buttonOpenTmpWindow, tmpA.children[1])
