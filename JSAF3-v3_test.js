a231fa40b99247b6460261bb635b659995a903a3
grumstv <grumstv@gmail.com>
2021-07-09 19:33:29 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -67,7 +67,7 @@ var win_AFhelper =
 					<button id="hideMenu" style="margin-left: 10px">hide</button>
 					<button id="setting" style="margin-left: 15px">S</button>
 					<button id="addsrc" style="margin-left: 5px">*</button>
-					<button id="links" style="margin-left: 5px">L</button>
+					<button id="links" style="margin-left: 30px">L</button>
 					<input id ="phone_tr" placeholder="Телефон" autocomplete="off" type="text" style = "text-align: center; width: 120px; color: black; margin-left: 12px"></input>
                     			<input id ="email_tr" placeholder="Почта" autocomplete="off" type="text" style = "text-align: center; width: 120px; color: black; margin-left: 10px"></input>
 				</div>
