fde68d00d386fec31908e9e7c37af6dc70243b1a
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2021-07-20 17:53:31 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -367,12 +367,17 @@ wintLinks.style.display = 'none';
 wintLinks.setAttribute('id' ,'AF_Links');
 wintLinks.innerHTML = win_Links; 
  document.getElementById('hash_type_for_chat').style.display = 'none'; // скрытие поиска id по хэшу
+ document.getElementById('btn1_hash').style.display = 'none';
  document.getElementById('main_easy_win').ondblclick = function () {
-	if(document.getElementById('hash_type_for_chat').style.display == 'none')
+	if(document.getElementById('hash_type_for_chat').style.display == 'none') {
 		document.getElementById('hash_type_for_chat').style.display = '';
-	else
-		document.getElementById('hash_type_for_chat').style.display = 'none';
+		document.getElementById('btn1_hash').style.display = '';
 	}
+	else {
+		document.getElementById('hash_type_for_chat').style.display = 'none';
+		document.getElementById('btn1_hash').style.display = 'none';
+		}
+ 	}
 
  var listener4 = function(e , a) { // сохранение последней позиции окна ссылок
         wintLinks.style.left = Number(e.clientX - myX4) + "px";
