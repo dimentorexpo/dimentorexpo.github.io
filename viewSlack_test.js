3dd73625fafe164b464d002f6a58697bcd15c437
grumstv <grumstv@gmail.com>
2021-07-25 19:09:22 +0300

    Update viewSlack_test.js

--- viewSlack_test.js
+++ viewSlack_test.js
@@ -124,7 +124,7 @@ function createSlackView() {
 	let client_token = Number(new Date())
 	requestOptions = {
 	  	  "headers": {
-		"content-type": "multipart/form-data; boundary=----WebKitFormBoundaryyKJPTlPfb1YqBCtQ"",
+		"content-type": "multipart/form-data; boundary=----WebKitFormBoundaryyKJPTlPfb1YqBCtQ",
 	  },
 	  "body": "------WebKitFormBoundaryyKJPTlPfb1YqBCtQ\r\nContent-Disposition: form-data; name=\"action_id\"\r\n\r\nAa013T6RBZSN\r\n------WebKitFormBoundaryyKJPTlPfb1YqBCtQ\r\nContent-Disposition: form-data; name=\"app_id\"\r\n\r\nA014EAVN8SU\r\n------WebKitFormBoundaryyKJPTlPfb1YqBCtQ\r\nContent-Disposition: form-data; name=\"client_token\"\r\n\r\nweb-" + client_token + "\r\n------WebKitFormBoundaryyKJPTlPfb1YqBCtQ\r\nContent-Disposition: form-data; name=\"token\"\r\n\r\n" + localStorage.getItem('token') + "\r\n------WebKitFormBoundaryyKJPTlPfb1YqBCtQ\r\n",
 	  "method": "POST",
