7c618e20e2aa7b9e0198ac6b53d7f41b85949549
grumstv <grumstv@gmail.com>
2021-07-27 01:12:43 +0300

    Update viewSlack_test.js

--- viewSlack_test.js
+++ viewSlack_test.js
@@ -315,7 +315,7 @@ buttonOpenForm1.onclick = function() {
 	this.style.display = 'none'
 }
 var btnAdd = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
-btnAdd.insertBefore(buttonOpenForm, btnAdd.children[0])
+btnAdd.insertBefore(buttonOpenForm1, btnAdd.children[0])
 function submitSlackView(view) {
 	console.log(view)
 	let client_token = Number(new Date())
