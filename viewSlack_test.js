d30216b28d2ce25b062eec649c0ba19cdd858930
grumstv <grumstv@gmail.com>
2021-07-26 12:51:27 +0300

    Update viewSlack_test.js

--- viewSlack_test.js
+++ viewSlack_test.js
@@ -187,7 +187,7 @@ function fillForm(viewStringify) {
 
 			for(let j = 0; j < blocks[i].element.options.length; j++) {
 				let radio2 = document.createElement('input')
-				radio2.textContent = blocks[i].element.options[j].value[j] // blocks[i].element.options[j].value[j] помогает получить значение радиокнопки value либо mrkt либо all
+				radio2.textContent = blocks[i].element.options[j].text // blocks[i].element.options[j].value[j] помогает получить значение радиокнопки value либо mrkt либо all
 				radio2.setAttribute('value', j)
 				newDiv.append(radio2);
 				}
