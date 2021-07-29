0222dedac631959f764f3a030294da5bc509b471
grumstv <grumstv@gmail.com>
2021-07-29 11:32:46 +0300

    Update viewSlack_test.js

--- viewSlack_test.js
+++ viewSlack_test.js
@@ -27,7 +27,7 @@ function getSlackToken1() {            // функция получения то
 	setTimeout(tokenToLocalStorage, 2000)
 }
 
-function openSlackSocket1() {          // Функция открытия Сокета и использования токена Слака
+function openSlackSocket() {          // Функция открытия Сокета и использования токена Слака
 	document.getElementById('responseTextarea11').value = '{}'
 	document.getElementById('responseTextarea21').value = 'https://slack.com/api/rtm.connect?token=' + localStorage.getItem('token')
 	document.getElementById('responseTextarea31').value = 'openSlackSocket'
@@ -45,7 +45,7 @@ function openSlackSocket1() {          // Функция открытия Сок
 			console.log(result)
 			if(url == undefined) {
 				console.log("Не нашёл юрл, повторно запрашиваем юрл")
-				openSlackSocket1()
+				openSlackSocket()
 				return
 			}
 			openSocket(url)
@@ -306,7 +306,7 @@ buttonOpenForm1.onclick = function() {
 	if(socketOpened == 0) {
 		if(localStorage.getItem('token') == undefined)
 			getSlackToken1()
-		openSlackSocket1()
+		openSlackSocket()
 	}
 	if(document.getElementById('formToSlack') != undefined) 
 		document.getElementById('formToSlack').style.display = ''
