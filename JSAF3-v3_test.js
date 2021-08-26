03c4fc8b0365281795192714564d23f421e4505c
grumstv <grumstv@gmail.com>
2021-08-27 02:10:13 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -580,7 +580,7 @@ document.getElementById('pushmetotest').addEventListener('click',function(){
         "sec-fetch-user": "?1",
         "upgrade-insecure-requests": "1"
 },
-"body": "firstpartoflink + testInput.value + secondparftoflink",
+"body": `firstpartoflink + testInput.value + secondparftoflink`,
     "method": "POST",
     "mode": "cors",
     "credentials": "include"
