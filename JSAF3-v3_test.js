9e9892309a4e36c8ec0700ff26272d7c24d81739
grumstv <grumstv@gmail.com>
2021-08-27 02:06:31 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -565,6 +565,12 @@ const copyToClipboard = str => {           // инициализация фун�
 })
 
 document.getElementById('pushmetotest').addEventListener('click',function(){
+			let firstpartoflink = 'user_id_or_identity_for_one_time_password_form%5BuserIdOrIdentity%5D=';
+			let secondparftoflink = '&user_id_or_identity_for_one_time_password_form%5Bgenerate%5D=&user_id_or_identity_for_one_time_password_form%5B_token%5D=aRQybZDe-orjfAYST6y8VeHwML95ozQUJI8cadfN7gU'
+		if(testInput.value == "")
+			console.log('Введите id в поле')
+		else {
+	
 	document.getElementById('responseTextarea1').value = `{
 "headers": {
     "content-type": "application/x-www-form-urlencoded",
@@ -574,7 +580,7 @@ document.getElementById('pushmetotest').addEventListener('click',function(){
         "sec-fetch-user": "?1",
         "upgrade-insecure-requests": "1"
 },
-"body": "user_id_or_identity_for_one_time_password_form%5BuserIdOrIdentity%5D=57180&user_id_or_identity_for_one_time_password_form%5Bgenerate%5D=&user_id_or_identity_for_one_time_password_form%5B_token%5D=aRQybZDe-orjfAYST6y8VeHwML95ozQUJI8cadfN7gU",
+"body": "firstpartoflink + testInput.value + secondparftoflink",
     "method": "POST",
     "mode": "cors",
     "credentials": "include"
@@ -594,6 +600,8 @@ document.getElementById('responseTextarea3').value = ''
 console.log(convres[1]); }
 
 setTimeout(getPassInfo, 2000);
+			};
+			testInput.value = "";
 })
 
 
