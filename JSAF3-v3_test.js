b83b544abdc1ebc7e6869befe96c46aa7ac689e5
grumstv <grumstv@gmail.com>
2021-08-30 14:03:49 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -302,8 +302,14 @@ buttonmobpas.onclick = function() {
 var getidnewfromaf = 0;
 var newResult;
 buttonloc.onclick = function() {
+		    
+		for(i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if(document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
+         getidnewfromaf = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0];
+        console.log("getidnewfromaf = " + ' ' + getidnewfromaf);
+    }
 	
-			 document.getElementById('responseTextarea1').value = `{
+				 document.getElementById('responseTextarea1').value = `{
 				  "headers": {
 					"accept": "application/json, text/plain, */*",
 					"sec-ch-ua-mobile": "?0",
@@ -325,15 +331,9 @@ buttonloc.onclick = function() {
 	
 	        var statusResult = document.getElementById('responseTextarea1').getAttribute('statusofcrmprofile')
 			document.getElementById('responseTextarea1').removeAttribute('statusofcrmprofile');
-			newResult = statusResult.match(/serviceLocale.*?([a-z]{4})/)[1];
+			newResult = statusResult.match(/serviceLocale.*?([a-z]{4})/);
 		
-		if (newResult == "null") {
-	    
-		for(i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
-        if(document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
-         getidnewfromaf = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0];
-        console.log("getidnewfromaf = " + ' ' + getidnewfromaf);
-    }
+		if (newResult[1] == "null") {
 	
 	 document.getElementById('responseTextarea1').value = `{
 		   "headers": {
