26d5fbdb4db7a548a063707f0a3b0f4373fcf31e
grumstv <grumstv@gmail.com>
2021-08-30 13:50:47 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -324,8 +324,9 @@ buttonloc.onclick = function() {
 	
 	        var statusResult = document.getElementById('responseTextarea1').getAttribute('statusofcrmprofile')
         document.getElementById('responseTextarea1').removeAttribute('statusofcrmprofile');
+		var newResult = statusResult.match(/serviceLocale.*?([a-z]{4})/)[1];
 		
-		if (statusResult.match(/serviceLocale.*?([a-z]{4})/)[1] == "null") {
+		if (newResult == "null") {
 	    
 		for(i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
         if(document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
@@ -353,11 +354,10 @@ buttonloc.onclick = function() {
 	 document.getElementById('sendResponse').click()
 	 
 	 	document.getElementById("buttonloc").innerHTML = "Локаль успешно изменена";
-		} else 
-			{  if (statusResult.match(/serviceLocale.*?([a-z]{2})/)[1] == "ru") {
-					document.getElementById("buttonloc").innerHTML = "Локаль уже задана";
+		} else 	{  	
+			document.getElementById("buttonloc").innerHTML = "Локаль уже задана";
 		}
-		}
+		
 }
 
 
