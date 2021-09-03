5c34b7dc107b7694e41ef417a030f8a621361d92
grumstv <grumstv@gmail.com>
2021-09-04 00:58:40 +0300

    Добавил сохранение таймера при обновлении страницы
    Теперь даже при обновлении страницы таймер сработает!

--- JSAF3-v3.js
+++ JSAF3-v3.js
@@ -230,7 +230,6 @@ buttonmobpas.innerHTML = '<a style="color: black; cursor: pointer;">Generate cod
 let button22 = document.createElement('p');
 button22.id = 'userShowcaseScript';
 button22.innerHTML = "Showcase";
-
 let button3 = document.createElement('p');
 button3.id = 'nextStudentIdScript';
 button3.innerHTML = "Info";
@@ -894,22 +893,20 @@ document.getElementById('benchmark').onclick = function () {                  //
     cpuname.value = "";
 }
 
-let chronostamp;
 document.getElementById('setreminder').onclick = function () {                  // выставляем будильник
+			localStorage.setItem('setchas', setchas.value);
+			localStorage.setItem('setminuta', setminuta.value);
 			var timearr = new Date()
-			let chas = setchas.value;
-			let minutka = setminuta.value;
-			const secunda = 0;
-			let difchas = chas - timearr.getHours();
-			let difmin =  minutka - timearr.getMinutes();
-			let difsec= secunda - timearr.getSeconds();
-			chronostamp = ((difchas * 60 * 60) + (difmin * 60) + difsec) * 1000
-			setchas.value = "";
-			setminuta.value = "";
-			alert("Будильник установлен на" + chas + ":" + minutka + ":"  + "0" + secunda);
+			var chronostamp = (((localStorage.getItem('setchas') - timearr.getHours()) * 60 * 60) + ((localStorage.getItem('setminuta') - timearr.getMinutes()) * 60) + (-10 - timearr.getSeconds())) * 1000;
+			localStorage.setItem('chronostamp', chronostamp);
+	//		setchas.value = "";
+	//		setminuta.value = "";
+			alert("Будильник установлен на" + setchas.value + ":" + setminuta.value + ":"  + "00");	
+}
+
 			function setRemindAf() {
-				document.getElementsByClassName("ant-btn ant-dropdown-trigger")[1].style.backgroundColor = "orange";
-				fetch("https://skyeng.autofaq.ai/api/reason8/operator/status", {
+		document.getElementsByClassName("ant-btn ant-dropdown-trigger")[1].style.backgroundColor = "orange";
+		fetch("https://skyeng.autofaq.ai/api/reason8/operator/status", {
 		  "headers": {
 			"accept": "*/*",
 			"cache-control": "max-age=0",
@@ -928,9 +925,9 @@ document.getElementById('setreminder').onclick = function () {                  
 			});							
 			alert("Время ставить занят!");
 	}
-	setTimeout(setRemindAf, chronostamp);
-}
-
+	
+	setTimeout(setRemindAf, localStorage.getItem('chronostamp'));
+	
 
 
 document.getElementById('groupadm').onclick = function () {                     //переход в админку редактора группы
