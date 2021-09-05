27da4959b42c4eb8e179147151ef90a3f6349845
grumstv <grumstv@gmail.com>
2021-09-05 21:55:52 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -734,9 +734,21 @@ function move_again_AF() {
 			time = "00" + " : " + "00" + " : " + "00" ;
 			document.getElementById("clock_remin").innerHTML = time;
 		} else if (((localStorage.getItem('setchas') - hours) == 0) && ((localStorage.getItem('setminuta') - minutes) >= 0)) {
-        time = "00" + " : " + (localStorage.getItem('setminuta') - minutes-1) + " : " + (60 - seconds);
-        document.getElementById("clock_remin").innerHTML = time;
-			} else if (localStorage.getItem('setchas') - hours < 0 || localStorage.getItem('setminuta') - minutes < 0) {
+			time = "00" + " : " + (localStorage.getItem('setminuta') - minutes-1) + " : " + (60 - seconds);
+			document.getElementById("clock_remin").innerHTML = time;
+			} else if (((localStorage.getItem('setchas') - hours) >= 1) && ((localStorage.getItem('setminuta') - minutes) >= 0)) {
+				time = ((localStorage.getItem('setchas') - hours) - 1) + " : " + (localStorage.getItem('setminuta') - minutes-1) + " : " + (60 - seconds);
+				document.getElementById("clock_remin").innerHTML = time;
+			} else if (((localStorage.getItem('setchas') - hours) >= 1) && ((localStorage.getItem('setminuta') - minutes) == 0)) {
+				time = ((localStorage.getItem('setchas') - hours) - 1) + " : " + (localStorage.getItem('setminuta') - minutes+60) + " : " + (60 - seconds);
+				document.getElementById("clock_remin").innerHTML = time;
+			} else if (((localStorage.getItem('setchas') - hours) >= 1) && localStorage.getItem('setminuta') <= minutes) {
+				time = ((localStorage.getItem('setchas') - hours) - 1) + " : " + (localStorage.getItem('setminuta') + 60-minutes) + " : " + (60 - seconds);
+				document.getElementById("clock_remin").innerHTML = time;
+			} else if (((localStorage.getItem('setchas') - hours) >= 1) && localStorage.getItem('setminuta') > minutes) {
+				time = (localStorage.getItem('setchas') - hours) + " : " + (localStorage.getItem('setminuta') - minutes) + " : " + (60 - seconds);
+				document.getElementById("clock_remin").innerHTML = time;
+			} else {			
 				time = "00" + " : " + "00" + " : " + "00"; 
 				document.getElementById("clock_remin").innerHTML = time;
 				}
