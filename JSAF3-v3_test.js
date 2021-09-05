57d62f09ea45adfc594993986ef08bc928df1557
grumstv <grumstv@gmail.com>
2021-09-05 20:37:32 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -727,14 +727,13 @@ function move_again_AF() {
         hours = data.getHours();
         minutes = data.getMinutes();
         seconds = data.getSeconds();
-		if (localStorage.getItem('setchas') - hours < 0 || localStorage.getItem('setminuta') - minutes < 0) {
-			time = "00" + " : " + "00" + " : " + "00"; 
-			document.getElementById("clock_remin").innerHTML = time;
-			} else if (localStorage.getItem('setchas') - hours >= 0 || localStorage.getItem('setminuta') - minutes >= 0) {
+		time = "00" + " : " + "00" + " : " + "00"; 
+		document.getElementById("clock_remin").innerHTML = time;
+		if (localStorage.getItem('setchas') - hours >= 0 || localStorage.getItem('setminuta') - minutes >= 0) {
 				time = "0" + (localStorage.getItem('setchas') - hours) + " : " + "0" + (localStorage.getItem('setminuta') - minutes-1) + " : " + (60 - seconds);
 				document.getElementById("clock_remin").innerHTML = time; 
 			} 
-			}
+		}
 
 
     document.getElementById('kibanalnksvz').addEventListener('click',function(){
