4b8659473cfe6f60e8d9087471ef144ea1780045
grumstv <grumstv@gmail.com>
2021-09-05 17:27:33 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -733,11 +733,14 @@ function move_again_AF() {
 		if (localStorage.getItem('setchas') - hours < 0 || localStorage.getItem('setminuta') - minutes < 0) {
 			time = "00" + " : " + "00" + " : " + "00";
 			document.getElementById("clock_remin").innerHTML = time;
-			} else if (localStorage.getItem('setchas') - hours >= 0 || localStorage.getItem('setminuta') - minutes >= 0) {
-        time = (localStorage.getItem('setchas') - hours) + " : " + (localStorage.getItem('setminuta') - minutes-1) + " : " + (60 - seconds);
-        document.getElementById("clock_remin").innerHTML = time;
+			} else if (localStorage.getItem('setchas') - hours >= 0 && localStorage.getItem('setchas') - hours == 1 || localStorage.getItem('setminuta') - minutes >= 0) {
+				time = (localStorage.getItem('setchas') - hours-1) + " : " + (localStorage.getItem('setminuta') - minutes-1) + " : " + (60 - seconds);
+			document.getElementById("clock_remin").innerHTML = time; 
+			} else if (localStorage.getItem('setchas') - hours >= 0 && localStorage.getItem('setchas') - hours > 1 || localStorage.getItem('setminuta') - minutes >= 0) {
+				time = (localStorage.getItem('setchas') - hours) + " : " + (localStorage.getItem('setminuta') - minutes-1) + " : " + (60 - seconds);
 			if (localStorage.getItem('setchas') - hours < 0 || localStorage.getItem('setminuta') - minutes < 0)
 				time = "00" + " : " + "00" + " : " + "00";
+				
 			}
 		}
 
