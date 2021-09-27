01c33f6a7390ccf12120f3bdf0545c4809d3201a
grumstv <grumstv@gmail.com>
2021-09-27 10:51:28 +0300

    Update JSAF3-v3.js

--- JSAF3-v3.js
+++ JSAF3-v3.js
@@ -10,11 +10,6 @@ function mystyles() {
 		padding: 3px 2px;
 		margin: -2px 1px;
 	}
-	
-	.ant-btn.expert-get_ticket.ant-btn-block {
-		background-color: #fff !important;
-	}
-	
 	button {
 		background-color:#768d87;
 		border-radius:5px; 
@@ -103,7 +98,7 @@ var win_AFhelper =  // описание элементов главного ок
 				<input id="setminuta" placeholder="MM" autocomplete="off" type="text" style="text-align: center; margin-top: 5px;  width: 30px; color: black;">
 				<button id="setreminder" style="margin-top: 5px">SET🔔</button>
 				<br>
-				<button id="curVeriOS" style="margin-top: 5px">iOS: 9.34</button>
+				<button id="curVeriOS" style="margin-top: 5px">iOS: 9.35</button>
 				<button id="curVerAndroid" style="margin-top: 5px">Аndroid: 9.32</button>
 				<br>
 				<button id="clock_js" style="color: white; margin-top: 5px"></button>
@@ -3571,8 +3566,6 @@ function textToUTF8String(string) {
     }
     return string2
 }
-
-document.getElementsByClassName('ant-btn expert-get_ticket ant-btn-block')[0].style.backgroundColor = "white"
 
 function weWillNotBeSlaves() {
     localStorage.setItem('scriptAdr', TP_addr2)
