254be9136784a2e0d9458b2afe0318af002292a1
grumstv <grumstv@gmail.com>
2021-09-29 14:28:53 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1472,17 +1472,17 @@ async function buttonsFromDoc(butName) {
             adr = adr1 = uid = ""
             var values = await getInfo(0).then(values => { adr = values[0]; adr1 = values[1]; uid = values[2]; });
 
-/*count = await checkHistory(uid.split(',')[0])
+            count = await checkHistory(uid.split(',')[0])
             if (count > 1 && flagggg == 0) {
                 if (document.getElementById('languageAF').innerHTML == "Русский") {
                     if (localStorage.getItem('scriptAdr') == TP_addr || localStorage.getItem('scriptAdr') == TP_addr2)
-                        txt = "Просматриваю информацию по вашему запросу. Вернусь с ответом или за уточнениями через несколько минут. Please wait a few minutes."
+                        txt = "Здравствуйте! Просматриваю информацию по вашему запросу. Вернусь с ответом или за уточнениями через несколько минут. Please wait a few minutes."
                 }
                 else {
                     if (localStorage.getItem('scriptAdr') == TP_addr || localStorage.getItem('scriptAdr') == TP_addr2)
-                        txt = "Please wait a few minutes."
+                        txt = "Hello. Please wait a few minutes."
                 }
-            } else {*/
+            } else {
                 flagggg = 0
                 a = document.getElementsByClassName('expert-user_info_panel')[0].firstChild.firstChild.innerText
                 a = a.split(' ')
@@ -1502,7 +1502,7 @@ async function buttonsFromDoc(butName) {
             else
                 txt = "Hello. Please wait a few minutes."
         }
-        if (txt == "Please wait a few minutes.")
+        if (txt == "Hello. Please wait a few minutes.")
             sendAnswer(txt)
         else
             sendAnswerTemplate2(txt)
