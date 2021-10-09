ca1d0e98689387c954a2d43d1e85f0ac4add3f86
grumstv <grumstv@gmail.com>
2021-10-10 01:23:34 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1132,33 +1132,38 @@ function move_again_AF() {
     }
 	         
 	
-		    document.getElementById('getJiraTasks').onclick = function () { 
-			 setTimeout(function getJiraTask() {
-                document.getElementById('responseTextarea1').value = `{
-					  "headers": {
-						 "__amdmodulename": "jira/issue/utils/xsrf-token-header",
-						"accept": "*/*",
-						 "sec-fetch-mode": "cors",
-						"sec-fetch-site": "same-origin",
-						"x-atlassian-token": "no-check",
-						"x-requested-with": "XMLHttpRequest"
-					  },
-					  "body": "startIndex=0&filterId=21266&jql=project+in+(VIM%2C+MP%2C+MV%2C+KIDS%2C+TS%2C+ADULT%2C+AUTH%2C+BILL%2C+COMM%2C+KG%2C+KIDSMOB%2C+MATH%2C+MOB%2C+MOBACK%2C+MOBT%2C+SS%2C+ST%2C+SMMOB%2C+STUDCAB)+AND+issuetype+in+(Bug%2C+Task)+AND+status+!%3D+closed+AND+Reports+%3E+0+AND+resolution+in+(Unresolved%2C+Incomplete%2C+%22Cannot+Reproduce%22)+AND+text+~%22+${testJira.value}+%22+ORDER+BY+updated&layoutKey=list-view",
-					  "method": "POST",
-					  "mode": "cors",
-					  "credentials": "include"
-				}`
-                document.getElementById('responseTextarea2').value = "https://jira.skyeng.tech/rest/issueNav/1/issueTable"
-                document.getElementById('responseTextarea3').value = 'getissuetable'
-				document.getElementById('sendResponse').click()
-				document.getElementById('AF_Jira').style.display = ''
-                let rezissuetable = JSON.parse(document.getElementById('responseTextarea1').getAttribute('getissuetable'))
-                document.getElementById('responseTextarea1').removeAttribute('getissuetable')
-				console.log(rezissuetable);
-				testJira.value = "";	
-				}, 3000)
+		document.getElementById('getJiraTasks').onclick = function () { 
+			setTimeout(function getJiraTask() {
+			   document.getElementById('responseTextarea1').value = `{
+					 "headers": {
+						"__amdmodulename": "jira/issue/utils/xsrf-token-header",
+					   "accept": "*/*",
+						"sec-fetch-mode": "cors",
+					   "sec-fetch-site": "same-origin",
+					   "x-atlassian-token": "no-check",
+					   "x-requested-with": "XMLHttpRequest"
+					 },
+					 "body": "startIndex=0&filterId=21266&jql=project+in+(VIM%2C+MP%2C+MV%2C+KIDS%2C+TS%2C+ADULT%2C+AUTH%2C+BILL%2C+COMM%2C+KG%2C+KIDSMOB%2C+MATH%2C+MOB%2C+MOBACK%2C+MOBT%2C+SS%2C+ST%2C+SMMOB%2C+STUDCAB)+AND+issuetype+in+(Bug%2C+Task)+AND+status+!%3D+closed+AND+Reports+%3E+0+AND+resolution+in+(Unresolved%2C+Incomplete%2C+%22Cannot+Reproduce%22)+AND+text+~%22+${testJira.value}+%22+ORDER+BY+updated&layoutKey=list-view",
+					 "method": "POST",
+					 "mode": "cors",
+					 "credentials": "include"
+			   }`
+			   document.getElementById('responseTextarea2').value = "https://jira.skyeng.tech/rest/issueNav/1/issueTable"
+			   document.getElementById('responseTextarea3').value = 'getissuetable'
+			   document.getElementById('sendResponse').click()
+				   
 
-			}      
+			   document.getElementById('AF_Jira').style.display = ''
+			   let rezissuetable = document.getElementById('responseTextarea1').getAttribute('getissuetable')
+								   if(rezissuetable == null)
+					   setTimeout(getJiraTask, 1000)
+					   else {
+			   rezissuetable = JSON.parse(rezissuetable)
+			   document.getElementById('responseTextarea1').removeAttribute('getissuetable')
+			   console.log(rezissuetable);
+			   testJira.value = "";	
+			}}, 500)
+		   }      
     
     document.getElementById('gotocrmoneinfo').onclick = function () {                  // проверка заявки ученика в СРМ1
         let crmonelnk = 'https://cabinet.skyeng.ru/orderV2/student/id/';
