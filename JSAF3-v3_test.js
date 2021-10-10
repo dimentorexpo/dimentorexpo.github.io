8b80cbb02df03f9a8a44829955d69b60672e4534
grumstv <grumstv@gmail.com>
2021-10-10 17:34:16 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1133,7 +1133,7 @@ function move_again_AF() {
 	         
 		document.getElementById('getJiraTasks').onclick = function () { 
 		let rezissuetable;
-			setTimeout(function getJiraTask() {
+		//	setTimeout(function getJiraTask() {
 			   document.getElementById('responseTextarea1').value = `{
 					 "headers": {
 						"__amdmodulename": "jira/issue/utils/xsrf-token-header",
@@ -1169,7 +1169,8 @@ function move_again_AF() {
 			  document.getElementById('issuetable').innerHTML = issues;
 			   console.log(rezissuetable.issueTable.issueKeys);
 			    setTimeout(function () { testJira.value= ""; rezissuetable=null; issues=[]; }, 10000)  	
-			}}, 500)			
+			}}
+			//, 500)			
 		   }      
     
     document.getElementById('gotocrmoneinfo').onclick = function () {                  // проверка заявки ученика в СРМ1
