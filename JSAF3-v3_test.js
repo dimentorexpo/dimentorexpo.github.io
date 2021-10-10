097f96a723adda3a276392a20d62a7a7bea4e385
grumstv <grumstv@gmail.com>
2021-10-10 14:28:57 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1164,22 +1164,7 @@ function move_again_AF() {
 			   let issues =[];
 			   let arraytmpl=[];
 			   for (let i=0; i<10; i++) {
-					setTimeout(function() { document.getElementById('responseTextarea1').value = `{}`
-											document.getElementById('responseTextarea2').value = "https://jira.skyeng.tech/secure/AjaxIssueAction!default.jspa?issueKey="+rezissuetable.issueTable.issueKeys[i]
-											document.getElementById('responseTextarea3').value = 'getlabels'					
-											document.getElementById('sendResponse').click()
-
-						let templabel = document.getElementById('responseTextarea1').getAttribute('getlabels');
-						templabel = JSON.parse(templabel)
-						arraytmpl += templabel;
-						document.getElementById('responseTextarea1').removeAttribute('getlabels')
-					//	arraytmpl += templabel
-											}, 1000)
-												console.log(arraytmpl)	
-											
-
-						
-	//			   issues += '<a href="https://jira.skyeng.tech/browse/' + rezissuetable.issueTable.issueKeys[i]  '" onclick="">' + rezissuetable.issueTable.issueKeys[i] + templabel.issue.summary[i]'</a></br>'			   
+					 issues += '<a href="https://jira.skyeng.tech/browse/' + rezissuetable.issueTable.issueKeys[i]  '" onclick="">' + rezissuetable.issueTable.issueKeys[i] + templabel.issue.summary[i]'</a></br>'			   
 			   }
 			  document.getElementById('issuetable').innerHTML = issues;
 			   console.log(rezissuetable.issueTable.issueKeys);
