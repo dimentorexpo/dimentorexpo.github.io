b19c79079adb1826d5b46b263327bec6525afafc
grumstv <grumstv@gmail.com>
2021-10-11 00:11:07 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1175,13 +1175,15 @@ function move_again_AF() {
 							if(rezissuetable.issueTable.issueKeys[i] !=undefined)				
 							issues += '<span style="color: #00FA9A">&#5129;</span>' + '<a href="https://jira.skyeng.tech/browse/' + rezissuetable.issueTable.issueKeys[i] + '" onclick="" target="_blank" style="color: #ffe4c4">' + rezissuetable.issueTable.table.match(/\w+\W+\d+">(\D+)<\/a>/gm)[i] + '</a>' + '<span id="sendascomment" class="sendcomments">💬</span></br>'
 						}
+						commentsarr = document.getElementsByClassName('sendcomments');
 						document.getElementById('issuetable').innerHTML = issues;
 						console.log(rezissuetable.issueTable.issueKeys);
 						setTimeout(function () { testJira.value= ""; issues=[]; }, 5000)}
 					
-					commentsarr = document.getElementsByClassName('sendcomments');
-						commentsarr.addEventListener('click', function () {
+						for(i=0;i<commentsarr.length;i++) {
+						commentsarr[i].addEventListener('click', function () {
 						console.log(commentsarr + " " + "clicked");
+						}
 						}
 						)
 						
