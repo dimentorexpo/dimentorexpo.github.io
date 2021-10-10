dbf1b9e99577e675458b2cb5071fe00f3058a453
grumstv <grumstv@gmail.com>
2021-10-11 00:43:13 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1173,7 +1173,7 @@ function move_again_AF() {
 						let issues =[];
 						for (let i=0; i<10; i++) {
 							if(rezissuetable.issueTable.issueKeys[i] !=undefined)				
-							issues += '<span style="color: #00FA9A">&#5129;</span>' + '<a href="https://jira.skyeng.tech/browse/' + rezissuetable.issueTable.issueKeys[i] + '" onclick="" target="_blank" style="color: #ffe4c4">' + rezissuetable.issueTable.table.match(/\w+\W+\d+">(\D+)<\/a>/gm)[i] + '</a>' + '<span onclick="sendComment("https://jira.skyeng.tech/browse/" + rezissuetable.issueTable.issueKeys[i]");" class="sendcomments">💬</span></br>'
+							issues += '<span style="color: #00FA9A">&#5129;</span>' + '<a href="https://jira.skyeng.tech/browse/' + rezissuetable.issueTable.issueKeys[i] + '" onclick="" target="_blank" style="color: #ffe4c4">' + rezissuetable.issueTable.table.match(/\w+\W+\d+">(\D+)<\/a>/gm)[i] + '</a>' + '<span onclick="sendComment("https://jira.skyeng.tech/browse/");" class="sendcomments">💬</span></br>'
 						}
 
 						commentsarr = document.getElementsByClassName('sendcomments');
