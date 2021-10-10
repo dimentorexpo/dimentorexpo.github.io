fc605a8e4e34fbcaf8f1641f1ba6f6574f1841b9
grumstv <grumstv@gmail.com>
2021-10-11 00:21:58 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1173,18 +1173,15 @@ function move_again_AF() {
 						let issues =[];
 						for (let i=0; i<10; i++) {
 							if(rezissuetable.issueTable.issueKeys[i] !=undefined)				
-							issues += '<span style="color: #00FA9A">&#5129;</span>' + '<a href="https://jira.skyeng.tech/browse/' + rezissuetable.issueTable.issueKeys[i] + '" onclick="" target="_blank" style="color: #ffe4c4">' + rezissuetable.issueTable.table.match(/\w+\W+\d+">(\D+)<\/a>/gm)[i] + '</a>' + '<span id="sendascomment" class="sendcomments">💬</span></br>'
+							issues += '<span style="color: #00FA9A">&#5129;</span>' + '<a href="https://jira.skyeng.tech/browse/' + rezissuetable.issueTable.issueKeys[i] + '" onclick="" target="_blank" style="color: #ffe4c4">' + rezissuetable.issueTable.table.match(/\w+\W+\d+">(\D+)<\/a>/gm)[i] + '</a>' + '<span onclick="commentClicked();" class="sendcomments">💬</span></br>'
 						}
 						commentsarr = document.getElementsByClassName('sendcomments');
 						document.getElementById('issuetable').innerHTML = issues;
 						console.log(rezissuetable.issueTable.issueKeys);
 						setTimeout(function () { testJira.value= ""; issues=[]; }, 5000)}
 					
-						for(i=0;i<commentsarr.length;i++) {
-							commentsarr[i].addEventListener('click', function () {
-								console.log(commentsarr + " " + "clicked");
-							}
-							)
+						function commentClicked() {
+							sendComment("https://jira.skyeng.tech/browse/"+rezissuetable.issueTable.issueKeys[i);
 						}
 					
 					document.getElementById('secondpage').addEventListener('click', function () {
