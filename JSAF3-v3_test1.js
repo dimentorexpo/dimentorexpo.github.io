6fa9689b3aa7e5cea13967c8139319ccf7fd0836
dimentorexpo <dimentor.expo11@gmail.com>
2021-10-14 22:18:41 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -991,8 +991,10 @@ function move_again_AF() {
     }
 
 	document.getElementById('clock_remin').ondblclick = function () {
-        if (localStorage.getItem('chronostamp') !== null && localStorage.getItem('chronostamp') > 0)
-		clearTimeout(abortTimeOut);
+        if (localStorage.getItem('chronostamp') !== null && localStorage.getItem('chronostamp') > 0){
+		clearTimeout(abortTimeOut)
+		alert("Будильник удален")
+	}
     }
 
     refreshTimerReminder();
