6eac88b4de98223551f847394abd94c0640b3b60
dimentorexpo <dimentor.expo11@gmail.com>
2021-10-14 22:59:18 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -992,6 +992,7 @@ function move_again_AF() {
 
 	document.getElementById('clock_remin').ondblclick = function () {		
         if (localStorage.getItem('chronostamp') !== null && localStorage.getItem('chronostamp') > 0){
+		clearTimeout(abortTimeOut)
 		localStorage.removeItem('chronostamp')
 		localStorage.removeItem('chronostamp2')
         	setchas.value = ""
