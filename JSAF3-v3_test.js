fdc43351dd05655f79b6d037c5ec2a02cb462ca0
grumstv <grumstv@gmail.com>
2021-10-15 19:25:07 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -322,13 +322,13 @@ gettacherphoto.onclick = function() {
 	    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
         if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
             getteacheridformaf = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0];
-        console.log("getidfromaf = " + ' ' + getidfromaf);
+        console.log("getteacheridformaf = " + ' ' + getteacheridformaf);
     }
 	
-    document.getElementById('responseTextarea1').value = '{}'
-    document.getElementById('responseTextarea2').value = "https://skyeng.ru/teachers/details/"+getteacheridformaf
-    document.getElementById('responseTextarea3').value = 'imageurl'
-    document.getElementById('sendResponse').click()
+		document.getElementById('responseTextarea1').value = '{}'
+		document.getElementById('responseTextarea2').value = "https://skyeng.ru/teachers/details/"+getteacheridformaf
+		document.getElementById('responseTextarea3').value = 'imageurl'
+		document.getElementById('sendResponse').click()
 	
 	    function getImageUrl() {
         document.getElementById('responseTextarea1').value = '{}'
@@ -336,8 +336,8 @@ gettacherphoto.onclick = function() {
         document.getElementById('responseTextarea3').value = 'imageurl'
 
         var rezresp = document.getElementById('responseTextarea1').getAttribute('imageurl')
-        document.getElementById('responseTextarea1').removeAttribute('imageurl');
-        var convertrezresp= rezresp.match(/(https:\/\/auth-avatars-skyeng.imgix.net.*?\d+.\S+).auto/);
+        var convertrezresp= rezresp.match(/(https:\/\/auth-avatars-skyeng.imgix.net.*?\d+.\S+).auto/)[1];
+		document.getElementById('responseTextarea1').removeAttribute('imageurl');
 		teacherphoto.src = convertrezresp;
     }
     setTimeout(getImageUrl, 1000);
