2fd99374fb983c07d3721c2222d0e1fb3b72f806
grumstv <grumstv@gmail.com>
2021-10-21 22:36:36 +0300

    для исхходного обратившегося также фикс

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -315,13 +315,22 @@ button2.onclick = function () { //функция Info по нажатию на �
 }
 
 buttonhistory.onclick = function () { //функция приска пр истории чатов в коте
+if (document.querySelector('#hide_or_display').textContent != "свернуть") {
     hide_or_display.click()
     for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
         if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
             document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0]
     }
     search.click()
+} else if (document.querySelector('#hide_or_display').textContent == "свернуть") {
+    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
+            document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0]
+    }
+	search.click()
 }
+}
+
 
 let getteacheridformaf;
 gettacherphoto.onclick = function() {  //функция добычи фото П из ТРМ и отображении в АФ по нажатию на Get photo
