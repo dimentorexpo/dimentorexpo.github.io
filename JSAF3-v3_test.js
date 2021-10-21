2a217cd7b86d4907e135ac68da4dac988e3e89b8
grumstv <grumstv@gmail.com>
2021-10-21 21:58:03 +0300

    Revert "Пробую вернут ьсервернрые"
    This reverts commit af44d994265c32ec5c5c55d80bb1162b0a3b6e97.

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1925,13 +1925,13 @@ async function buttonsFromDoc(butName) {
         return
     
 	   }
-}
 
-function servFromDoc(butName) {
-    but = butName
-    msgFromTable(but)
-    if (document.getElementById('avariyalink').value !== null)
-        sendComment(document.getElementById('avariyalink').value);
+    msgFromTable(butName)
+    if (butName == "Серверные")
+        if (document.getElementById('msg1').innerHTML != "Доработать") {
+            sendComment(document.getElementById('serversInp').value)
+            newTag(1370)
+        }
 }
 
 var bool = 0;
@@ -2029,27 +2029,6 @@ function refreshTemplates() {
                 countOfPages++
 
                 countOfStr = 1
-				
-				
-                if(pageType == "Серверные") {
-					var newDiv = document.createElement('div')
-					newDiv.id = countOfPages + "page_" + countOfStr + "str"
-					newDiv.style.margin = "5px"
-
-					var newInputAlink = document.createElement('input')
-					newInputAlink.id = 'avariyalink'
-					newInputAlink.placeholder = 'Ссылка на трэд или Jira северных'
-					newInputAlink.autocomplete = 'off'
-					newInputAlink.type = 'text'
-					newInputAlink.style = 'text-align: center; width: 300px; color: black; margin-left: 20px'
-
-					newDiv.appendChild(newInputAlink)
-
-					b.lastElementChild.appendChild(newDiv)
-					countOfStr++
-				}
-
-				
                 var newStr = document.createElement('div')
                 newStr.style.margin = "5px"
                 newStr.id = countOfPages + "page_" + countOfStr + "str"
@@ -2084,7 +2063,20 @@ function refreshTemplates() {
                         if (newBut.innerText == 'ус+брауз (У)')
                             newBut.innerText = "ус+брауз"
                         if (newBut.innerText == 'ус+брауз (П)')
-                          	continue 
+                            continue
+                        if (newBut.innerText == 'Серверные') {
+                            var newInput = document.createElement('input')
+                            newInput.placeholder = 'Ссылка'
+                            newInput.id = 'serversInp'
+                            newInput.style.marginRight = '5px'
+                            var newDiv = document.createElement('div')
+                            newDiv.style.margin = '5px'
+                            newBut.id = 'serversBut'
+                            newDiv.append(newInput)
+                            newDiv.append(newBut)
+                            document.getElementById('addTmp').children[0].appendChild(newDiv)
+                            continue
+                        }
                         if (addTmpFlag == 0)
                             b.lastElementChild.lastElementChild.appendChild(newBut)
                         else {
@@ -2092,14 +2084,12 @@ function refreshTemplates() {
                             document.getElementById('addTmp').children[0].appendChild(newBut)
                         }
                         break
-                    case 'Серверные':
-                            var newBut = document.createElement('button')
-                            newBut.innerText = c[0]
-                            newBut.style.marginRight = '4px'
-                            newBut.setAttribute('onclick', 'servFromDoc(this.innerText)')
-                            b.lastElementChild.lastElementChild.appendChild(newBut)
+                    case 'Переводы':
+                        var newBut = document.createElement('button')
+                        newBut.innerText = c[0]
+                        newBut.style.marginRight = '4px'
+                        b.lastElementChild.lastElementChild.appendChild(newBut)
                         break
-
                     case 'Темы':
                         var newBut = document.createElement('button')
                         newBut.innerText = c[0]
