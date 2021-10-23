1ddd0a71a46a2d8afa6d159e6972d4882332f927
grumstv <grumstv@gmail.com>
2021-10-23 16:07:15 +0300

    Дубль 2 вернуть кнопку гет фото

--- JSAF3-v3.js
+++ JSAF3-v3.js
@@ -264,6 +264,13 @@ button3.innerHTML = "Info";
 let button4 = document.createElement('p');
 button4.id = 'nextTeacherIdScript';
 button4.innerHTML = '<a style="color: black; cursor: pointer;">Info</a>';
+let gettacherphoto = document.createElement('p');
+gettacherphoto.id = 'getphototeacher';
+gettacherphoto.innerHTML = '<a style="color: black; cursor: pointer;">Get photo</a>';
+let teacherphoto = document.createElement('img');
+teacherphoto.id = 'URLphoto';
+teacherphoto.style.width = "150px";
+teacherphoto.style.height = "180px";
 
 
 
@@ -302,6 +309,39 @@ button4.onclick = function () {
             document.getElementById('id_type_for_chat').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText
     }
     btn1_student.click()
+}
+
+let getteacheridformaf;
+gettacherphoto.onclick = function() {
+	//	document.getElementById('getphototeacher').textContent="Скрыть фото";
+	    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
+            getteacheridformaf = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0];
+        console.log("getteacheridformaf = " + ' ' + getteacheridformaf);
+    }
+
+		document.getElementById('responseTextarea1').value = '{}'
+		document.getElementById('responseTextarea2').value = "https://skyeng.ru/teachers/details/"+getteacheridformaf
+		document.getElementById('responseTextarea3').value = 'imageurl'
+		document.getElementById('sendResponse').click()
+
+	    function getImageUrl() {
+        document.getElementById('responseTextarea1').value = '{}'
+        document.getElementById('responseTextarea2').value = "https://skyeng.ru/teachers/details/"+getteacheridformaf
+        document.getElementById('responseTextarea3').value = 'imageurl'
+
+        var rezresp = document.getElementById('responseTextarea1').getAttribute('imageurl')
+        var convertrezresp= rezresp.match(/(https:\/\/auth-avatars-skyeng.imgix.net.*?\d+.\S+).auto/)[1];
+		document.getElementById('responseTextarea1').removeAttribute('imageurl');
+		teacherphoto.src = convertrezresp;
+    }
+    setTimeout(getImageUrl, 1000);
+
+	document.getElementById('getphototeacher').replaceWith(teacherphoto)
+
+	document.querySelector('#URLphoto').onclick = function() {
+	document.querySelector('#URLphoto').replaceWith(gettacherphoto)
+	}
 }
 
 
@@ -2431,6 +2471,17 @@ function startTimer() {
 								    document.getElementById('userTypeId').innerText = "(РУ)"
 									document.getElementById('userTypeId').style.color = "#DC143C"	
 								} 		
+					}
+					
+										for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) { //добавление кнопки Get Photo
+								if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].textContent == "teacher") {
+									  for (let i = 0; i < document.getElementsByClassName('expert-user_details-list')[1].childElementCount; i++) {
+											if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.textContent == "id")
+											{
+												document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].append(gettacherphoto)
+											}
+										}
+								}
 					}
 
                     c.append(a)
