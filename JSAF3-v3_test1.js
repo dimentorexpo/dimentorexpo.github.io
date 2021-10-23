d9b220bbb067f298305bb90fe9a7c1c02e55507d
dimentorexpo <dimentor.expo11@gmail.com>
2021-10-23 20:37:21 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -1346,10 +1346,9 @@ searchJiraByEnter.addEventListener('keydown', event => {
         }
     }
 
-    let dropmenu = document.createElement('button');
+    let dropmenu = document.createElement('div');
     dropmenu.id = 'scriptmenu';
     dropmenu.innerHTML = "Меню";
-    dropmenu.setAttribute('class','ant-btn ant-dropdown-trigger')
     dropmenu.style.marginRight = "15px";
     dropmenu.style.display = ''
     var btnAddd = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
