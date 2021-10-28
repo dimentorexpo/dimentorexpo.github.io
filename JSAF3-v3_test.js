70aec2a4b1e064bec02a1f2f1ddbbb93e5700cac
grumstv <grumstv@gmail.com>
2021-10-28 22:39:05 +0300

    еще правкаи пока отображения новой кнопки открывающей менюшку

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -68,7 +68,7 @@ var win_AFhelper =  // описание элементов главного ок
 					<button id="setting" style="width:16px; float: right; margin-right: 5px">S</button>
 					<button id="links" style="width:16px; float: right; margin-right: 5px">L</button>
 					<button id="addsrc" style="width:16px; float: right; margin-right: 5px">*</button>
-					<button id="servicestatus" style="width:16px; float: right; margin-right: 5px">&#9884;</button>
+					<button id="servicestatus" style="width:24px; float: right; margin-right: 5px">&#9884;</button>
 					<div id="reminderstatus" style="width:16px; float: right; margin-right: 5px"></div>
 					<input id ="phone_tr" placeholder="Телефон" autocomplete="off" type="text" style = "text-align: center; width: 150px; color: black; margin-left: 15px; margin-top: 5px;"></input>
                     <input id ="email_tr" placeholder="Почта" autocomplete="off" type="text" style = "text-align: center; width: 150px; color: black; margin-left: 12px; margin-top: 5px;"></input>
