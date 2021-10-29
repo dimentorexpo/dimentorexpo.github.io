00217c45299baa2b9d8e040b90595d98aebae9ad
grumstv <grumstv@gmail.com>
2021-10-29 15:00:23 +0300

    Вернул чатхистори в общий скрипт

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -340,6 +340,15 @@ let teacherphoto = document.createElement('img');
 teacherphoto.id = 'URLphoto';
 teacherphoto.style.width = "150px";
 teacherphoto.style.height = "180px";
+let buttonhistory = document.createElement('p');
+buttonhistory.id = 'lookForHistory';
+buttonhistory.innerHTML = '<a style="color: black; cursor: pointer;">Chat History📋</a>';
+let buttonnextstudentid = document.createElement('p');
+buttonnextstudentid.id = 'nextStudentIdChatHistory';
+buttonnextstudentid.innerHTML = '<a style="color: black; cursor: pointer;">Chat History📋(У)</a>';
+let buttonnextteacherid = document.createElement('p');
+buttonnextteacherid.id = 'nextTeacherIdChatHistory';
+buttonnextteacherid.innerHTML = '<a style="color: black; cursor: pointer;">Chat History📋(П)</a>';
 
 
 
@@ -348,6 +357,58 @@ let template_flag2 = 0
 let word_text = ""
 let template_text = ""
 let flagggg = 0
+
+
+buttonhistory.onclick = function () { //функция приска пр истории чатов в коте
+if (document.querySelector('#hide_or_display').textContent != "свернуть") {
+    hide_or_display.click()
+    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
+            document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0]
+    }
+    search.click()
+} else if (document.querySelector('#hide_or_display').textContent == "свернуть") {
+    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
+            document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0]
+    }
+	search.click()
+}
+}
+
+buttonnextstudentid.onclick = function () {
+	if (document.querySelector('#hide_or_display').textContent != "свернуть") {
+    hide_or_display.click()
+    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "nextClass-studentId")
+            document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText
+    }
+	search.click()
+ } else if (document.querySelector('#hide_or_display').textContent == "свернуть") {
+    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "nextClass-studentId")
+            document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText
+    }
+	search.click()
+}
+}
+
+buttonnextteacherid.onclick = function () {
+	if (document.querySelector('#hide_or_display').textContent != "свернуть") {
+    hide_or_display.click()
+    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "nextClass-teacherId")
+            document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText
+    }
+	search.click()
+ } else if (document.querySelector('#hide_or_display').textContent == "свернуть") {
+    for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+        if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "nextClass-teacherId")
+            document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText
+    }
+	search.click()
+}
+}
 
 button2.onclick = function () { //функция Info по нажатию на которую ID переносится в расширение омельченко и нажимает Info кнопку автоматически
     if (document.getElementById('btn_hide').style.display != 'none')
