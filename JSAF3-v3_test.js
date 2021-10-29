55352cc946fe0b88739cad3c8447e2ebd5e1ecb1
grumstv <grumstv@gmail.com>
2021-10-29 21:11:49 +0300

    правкаи

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -384,7 +384,7 @@ buttonsetstudentidandservicetouserfield.onclick = function() {
 					  for (let j = 0; j < document.getElementsByClassName('expert-user_details-list')[1].childElementCount; j++) {
 							if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[j].firstChild.textContent == "id")
 										{
-											let getidusrstud = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0];
+											let getidusrstud = document.getElementsByClassName('expert-user_details-list')[1].childNodes[j].childNodes[1].innerText.split(' ')[0];
 											document.getElementsByClassName('ant-input-number-input')[0].value = getidusrstud;
 										}
 					  }
