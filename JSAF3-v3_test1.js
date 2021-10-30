418f426979412a4e2ab74660b627a77205034b90
dimentorexpo <dimentor.expo11@gmail.com>
2021-10-30 14:31:30 +0300

    Update JSAF3-v3_test1.js
    menu

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -4167,7 +4167,7 @@ function firstLoadPage() {
 //            btnAdd1 = document.querySelectorAll('.app-body-content-user_menu')[0].childNodes[0]
 //            btnAdd1 = document.getElementsByClassName('app-body-content-user_menu')[0].children[0]
 //            btnAdd1.insertBefore(scrmn, btnAdd1.children[0])
-            btnAdd1.insertAdjacentElement('beforeBegin',scriptmenu)
+            btnAdd1.insertAdjacentElement('beforeBegin','scriptmenu')
             btnAdd1.insertBefore(hashBut, btnAdd1.children[0])
             btnAdd1.insertBefore(maskBack, btnAdd1.children[0])
 //            document.getElementById('scrmn').appendChild(scriptmenu)
