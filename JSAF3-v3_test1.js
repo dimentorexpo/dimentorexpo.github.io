cc7718bf51ced635adbbaec7a60f204c10975fbb
dimentorexpo <dimentor.expo11@gmail.com>
2021-10-30 15:37:33 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -297,7 +297,7 @@ var scriptmenu = // меню для ссылок
                 </svg>
             </span>
         </button>
-        <div id="Upmenu" class="ant-dropdown ant-dropdown-placement-bottomLeft " style=" displey: none; min-width: 110px; left: 1385px; top: 52px;">
+        <div id="upmenu" class="ant-dropdown ant-dropdown-placement-bottomLeft " style="min-width: 110px; left: 1385px; top: 52px;">
     	<ul class="ant-dropdown-menu ant-dropdown-menu-light ant-dropdown-menu-root ant-dropdown-menu-vertical" role="menu" tabindex="0">
 	    	<li class="ant-dropdown-menu-item ant-dropdown-menu-item-only-child" role="menuitem">Русский</li>
 		    <li class="ant-dropdown-menu-item ant-dropdown-menu-item-only-child" role="menuitem">English</li>
@@ -684,6 +684,7 @@ let scrptmn = document.createElement('div'); // создание окна ссы
 document.body.append(scriptmenu);
 scrptmn.setAttribute('id', 'scriptmen');
 scrptmn.innerHTML = scriptmenu;
+document.getElementById('upmenu').style.display = 'none';
 
 let wintLinks = document.createElement('div'); // создание окна ссылок
 document.body.append(wintLinks);
