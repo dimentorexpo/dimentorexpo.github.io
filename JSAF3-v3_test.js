8906cecc34b4718252610e42148d863311eb57cf
grumstv <grumstv@gmail.com>
2021-10-30 15:26:11 +0300

    trtt

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -2573,7 +2573,7 @@ function startTimer() {
 
             }
 			
-		if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].textContent != "teacher") {
+		if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].textContent != "teacher"  && document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id") {
 				btn = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i]
 				btn.appendChild(buttonservstud) 
 				
