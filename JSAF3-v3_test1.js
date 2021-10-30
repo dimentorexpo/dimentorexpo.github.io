7adfa2abfe47e06f64a55f68a18159bad7bc0e64
dimentorexpo <dimentor.expo11@gmail.com>
2021-10-30 17:08:36 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -289,7 +289,7 @@ var win_serviceinfo =  // описание элементов окна ссыл�
 
 var scriptmenu = // описание меню для ссылок
     `<div style="max-width: 110px"class="sc-AxhUy fxWvvr user_menu-language_switcher">
-        <button id="menubtn" type="button" class="ant-btn ant-dropdown-trigger ant-dropdown-open" ant-click-animating-without-extra-node="false">
+        <button id="menubtn" type="button" class="ant-btn ant-dropdown-trigger ant-dropdown-open" ant-click-animating-without-extra-node="false" >
             <span class="user_menu-status-name">Меню</span>
             <span role="img" aria-label="down" type="down" class="anticon anticon-down user_menu-dropdown-icon">
                 <svg viewBox="64 64 896 896" focusable="false" class="" data-icon="down" width="1em" height="1em" fill="currentColor" aria-hidden="true">
@@ -316,14 +316,15 @@ function maxLengthCheck(object) // функция ограничения кол-
       object.value = object.value.slice(0, object.maxLength)
   }
  
-
-document.getElementById('menubtn').onclick = function (){
+/*
+function openmenu (){
     if (document.getElementById("upmenu").style.display = 'none') {
         document.getElementById("upmenu").style.display = ''
     }else{
         document.getElementById("upmenu").style.display = 'none'
     }
 }
+*/
 
 document.getElementById()
 if (localStorage.getItem('winTopAF') == null) { // началоное положение главного окна (если не задано ранее)
@@ -575,26 +576,6 @@ hashBut.id = "hashBut"
 hashBut.innerHTML = "Хэш"
 hashBut.style.marginRight = "15px";
 
-/*
-function openmenu() {
-    document.getElementById("scriptmenu").classList.toggle("show");
-}
-
-
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-  
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
-*/
 
 let maskBack = document.createElement('div')
 maskBack.id = "maskBack"
@@ -693,6 +674,14 @@ let scrptmn = document.createElement('div'); // создание окна ссы
 document.body.append(scriptmenu);
 scrptmn.setAttribute('id', 'scriptmen');
 scrptmn.innerHTML = scriptmenu;
+
+document.getElementById('menubtn').onclick = function (){
+    if (document.getElementById("upmenu").style.display = 'none') {
+        document.getElementById("upmenu").style.display = ''
+    }else{
+        document.getElementById("upmenu").style.display = 'none'
+    }
+}
 
 let wintLinks = document.createElement('div'); // создание окна ссылок
 document.body.append(wintLinks);
