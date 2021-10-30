2dd105f0f4bd4f9bcd24d812f924200d4dea8c38
dimentorexpo <dimentor.expo11@gmail.com>
2021-10-30 17:59:33 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -288,17 +288,17 @@ var win_serviceinfo =  // описание элементов окна ссыл�
 </div>`;
 
 var scriptmenu = // описание меню для ссылок
-    `<div id="scriptmenua" style="max-width: 110px"class="sc-AxhUy fxWvvr user_menu-language_switcher">
-        <button id="menubtn" type="button" class="ant-btn ant-dropdown-trigger ant-dropdown-open" ant-click-animating-without-extra-node="false" onclick="openmenu()">
-            <span class="user_menu-status-name">Меню</span>
-            <span role="img" aria-label="down" type="down" class="anticon anticon-down user_menu-dropdown-icon">
-                <svg viewBox="64 64 896 896" focusable="false" class="" data-icon="down" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+    `<div id="scriptmenua" style="max-width: 110px"class="sc-AxhUy fxWvvr user_menu-language_switcher user_menu">
+        <button id="menubtn" type="button" class="ant-btn ant-dropdown-trigger ant-dropdown-open user_menu" ant-click-animating-without-extra-node="false" onclick="openmenu()">
+            <span class="user_menu-status-name user_menu">Меню</span>
+            <span role="img" aria-label="down" type="down" class="anticon anticon-down user_menu-dropdown-icon user_menu">
+                <svg viewBox="64 64 896 896" focusable="false" class="user_menu" data-icon="down" width="1em" height="1em" fill="currentColor" aria-hidden="true">
                     <path d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"></path>
                 </svg>
             </span>
         </button>
-        <div class="ant-dropdown ant-dropdown-placement-bottomLeft" style="min-width: 110px; left: 1055px; top: 52px;">
-    	<ul id="upmenu" style="display: none" class="ant-dropdown-menu ant-dropdown-menu-light ant-dropdown-menu-root ant-dropdown-menu-vertical" role="menu" tabindex="0">
+        <div class="ant-dropdown ant-dropdown-placement-bottomLeft user_menu" style="min-width: 110px; left: 1055px; top: 52px;">
+    	<ul id="upmenu" style="display: none" class="ant-dropdown-menu ant-dropdown-menu-light ant-dropdown-menu-root ant-dropdown-menu-vertical user_menu" role="menu" tabindex="0">
 	    	<li >Пункт1</li>
 		    <li >Пункт2</li>
 	    </ul> 
@@ -325,7 +325,7 @@ function openmenu(){
 }
 
 window.onclick = function(event) {
-    if (!event.target.matches('.scriptmenua')) {
+    if (!event.target.matches('.user_menu')) {
   
       var dropdowns = document.getElementsByClassName("ant-dropdown-menu");
       var i;
