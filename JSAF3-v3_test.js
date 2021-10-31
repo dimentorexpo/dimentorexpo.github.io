fbe3679bbd275d9cc33d63c8341ef0f2b8ce7b21
grumstv <grumstv@gmail.com>
2021-10-31 22:10:28 +0300

    test

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1280,7 +1280,6 @@ document.getElementById('getidstudent').onclick = function () {
 		let tempvarcrm = document.getElementById('idstudent').value;
 		let getcrmstatusinfo;
 		document.getElementById('CrmStatus').style.display ="none";
-		document.getElementById('getcurrentstatus').style.display ="none";
 		
 		 document.getElementById('responseTextarea1').value = `{
 				  "headers": {
