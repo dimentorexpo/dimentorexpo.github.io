919287a923bf529c8ef4e0a365bc2136b6f2a385
dimentorexpo <dimentor.expo11@gmail.com>
2021-11-01 21:08:33 +0200

    Update script_menu.js

--- script_menu.js
+++ script_menu.js
@@ -36,12 +36,10 @@ window.onclick = function(event) {
     }
   }
 
-  let scrptmn = document.createElement('div'); // создание окна ссылок
-  document.body.append(scriptmenu);
-  scrptmn.setAttribute('id', 'scriptmen');
-  scrptmn.innerHTML = scriptmenu;  
+let scrptmn = document.createElement('div'); // создание окна ссылок
+document.body.append(scriptmenu);
+scrptmn.setAttribute('id', 'scriptmen');
+scrptmn.innerHTML = scriptmenu;  
 
-  setTimeout(function () {
-    btnAdd1 = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
-    btnAdd1.insertBefore(scrptmn, btnAdd1.children[0])
-}, 2000)
+var btnAdd1 = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
+btnAdd1.insertBefore(scrptmn, btnAdd1.children[0])
