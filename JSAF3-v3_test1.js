b268347cef9793a3311d52dbbc70b49ef70ea0ce
dimentorexpo <dimentor.expo11@gmail.com>
2021-11-01 21:12:26 +0200

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -4124,7 +4124,10 @@ function firstLoadPage() {
         setTimeout(move_again_AF, 3500)
 
         setTimeout(function () {
-            btnAdd1 = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
+//            btnAdd1 = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
+//            btnAdd1.insertBefore(hashBut, btnAdd1.children[0])
+//            btnAdd1.insertBefore(maskBack, btnAdd1.children[0])
+            btnAdd1 = document.getElementsByid('upmenu')
             btnAdd1.insertBefore(hashBut, btnAdd1.children[0])
             btnAdd1.insertBefore(maskBack, btnAdd1.children[0])
         }, 2000)
