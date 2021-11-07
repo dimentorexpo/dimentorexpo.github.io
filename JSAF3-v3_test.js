1e1bb83eac4702dbc42324a6129cda09abc62254
grumstv <grumstv@gmail.com>
2021-11-07 22:52:52 +0300

    Небольшие правки орфографии

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1729,14 +1729,14 @@ document.getElementById('getidstudent').onclick = function () {
 				servinfo += [i+1] + ") " + '<span class = "iduslugitxt">ID Услуги: </span>' + servicearr.data[i].id + '<span class = "copyserviceid" style="margin-left: 5px; cursor: pointer">💾</span>' + '<br> Баланс: ' + servicearr.data[i].balance + '<br> STK: ' + servicearr.data[i].serviceTypeKey + '<hr style="width:260px; border: 1px dotted #ff0000;  border-style: none none dotted; color: #fff; background-color: #fff;">';
 			    arrservice += servicearr.data[i].id + ", "
 			} else if (servicearr.data[i].stage =="lost" && servicearr.data[i].incorrectnessReason == null ) {
-				tinfo = "Учитель отсутствует, услуга потеряна";
+				tinfo = "Учитель отсутствует, услуга(и) потеряна(ы)";
 				servinfo = "Услуга потеряна";
 			}  else if (servicearr.data[i].stage !="lost" && servicearr.data[i].incorrectnessReason != null ) {
-				tinfo = "Учитель отсутствует, услуга некорректна";
+				tinfo = "Учитель отсутствует, услуга(и) некорректна(ы)";
 				servinfo = "Услуга некорректна";
 			} else if (servicearr.data[i].stage =="lost" && servicearr.data[i].incorrectnessReason == null ) {
-				tinfo = "Учитель отсутствует, услуга потеряна или некорректна";
-				servinfo = "Услуга услуга потеряна или некорректна";
+				tinfo = "Учитель отсутствует, услуга(и) потеряна(ы) или некорректна(ы)";
+				servinfo = "Услуга услуга(и) потеряна(ы) или некорректна(ы)";
 			}  else if (servicearr.data == null) {
 				 noservinfo = 1;
 				
