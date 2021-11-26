cd24b8bea4e2cbd3d6e70165b123fc27eeb78878
dimentorexpo <dimentor.expo11@gmail.com>
2021-11-26 20:15:55 +0200

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -3958,10 +3958,9 @@ function timerHideButtons() {
                     document.getElementsByClassName('ant-modal-content')[0].children[2].children[i].style.display = 'none'
         
         if (document.getElementsByClassName('ant-modal-content')[0].children[1].children[0].childNodes[0].textContent == 'Создать задачу')
-            document.getElementsByClassName('ant-modal-content')[0].childNodes[1].style.backgroundColor = "#F34723"
-            //for (i = 1; i < document.getElementsByClassName('ant-modal-content')[0].children[2].childElementCount - 1; i++)
-            //    if (document.getElementsByClassName('ant-modal-content')[0].children[2].children[i].textContent != "Тех. поддержка V1")
-            //        document.getElementsByClassName('ant-modal-content')[0].children[2].children[i].style.display = 'none'
+            for (i = 1; i < document.getElementsByClassName('sc-fzqNJr hXQgjp')[0].children[2].childElementCount - 1; i++)
+                if (document.getElementsByClassName('ant-modal-content')[0].children[2].children[i].textContent != "Тех. поддержка V1")
+                    document.getElementsByClassName('ant-modal-content')[0].children[2].children[i].style.display = 'none'
     }
 }
 
