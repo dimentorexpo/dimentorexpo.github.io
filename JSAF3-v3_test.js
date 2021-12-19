3f6c76339703f0e9cb7a1a1db5afcaa9758bf9d0
grumstv <grumstv@gmail.com>
2021-12-19 09:50:41 +0300

    testim без функции получения токена

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -2098,48 +2098,6 @@ function move_again_AF() {
     }
 
     let tokenlogginer;
-    async function getlogginer() {
-        //Для получения токена сначала обрабатываем
-        document.getElementById('responseTextarea1').value = `{
-					  "referrer": "https://id.skyeng.ru/admin/users",
-					  "referrerPolicy": "strict-origin-when-cross-origin",
-					  "method": "GET",
-					  "mode": "cors",
-					  "credentials": "include"
-	}`
-        document.getElementById('responseTextarea2').value = "https://id.skyeng.ru/admin/auth/login-links";
-        document.getElementById('responseTextarea3').value = 'gettoken'
-        document.getElementById('sendResponse').click()
-
-
-        async function gettokenlog() {
-
-            document.getElementById('responseTextarea1').value = `{
-					  "referrer": "https://id.skyeng.ru/admin/users",
-					  "referrerPolicy": "strict-origin-when-cross-origin",
-					  "method": "GET",
-					  "mode": "cors",
-					  "credentials": "include"
-	}`
-            document.getElementById('responseTextarea2').value = "https://id.skyeng.ru/admin/auth/login-links";
-            document.getElementById('responseTextarea3').value = 'gettoken'
-            document.getElementById('sendResponse').click()
-
-
-            tokenlogginer = document.getElementById('responseTextarea1').getAttribute('gettoken');
-            tokenlogginer = await tokenlogginer;
-            let dokie = document.createElement('div');
-            dokie.innerHTML = tokenlogginer;
-            tokenlogginer = dokie.querySelector('#login_link_form__token').value;
-            //console.log("Tokenlogginer: " + tokenlogginer)
-            document.getElementById('responseTextarea1').removeAttribute('gettoken');
-
-        }
-
-        setTimeout(gettokenlog, 500)
-    }
-
-
     let logginerinfo;
     async function postuderdatatologin() {
         let useriddata = document.getElementById('idstudent').value;
@@ -2447,7 +2405,6 @@ function move_again_AF() {
 			}
 		}, 1000)
 
-           setTimeout(getlogginer, 730);
      //   await getusernamecrm();
      //   await getuseragecrm();
         // await getunhideemail();
