5486327e28b5f774d20a7db166a4fb482a4e71df
grumstv <grumstv@gmail.com>
2022-01-17 08:37:07 +0300

    Пробуем разбить на 2 файла

---
+++ ServiceDesk.js
@@ -0,0 +1,165 @@
+	document.getElementById('servDsk').onclick = function () {
+        if (document.getElementById('AF_ServDsk').style.display == '')
+            document.getElementById('AF_ServDsk').style.display = 'none'
+        else
+            document.getElementById('AF_ServDsk').style.display = ''
+			document.getElementById('optionTeacher').onclick = function() {
+				if (document.getElementById('teacherssrvdskoptions').style.display != ''){
+					document.getElementById('teacherssrvdskoptions').style.display = '';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+		 }
+		 
+		 			document.getElementById('optionSkysmart').onclick = function() {
+				if (document.getElementById('skysmartsrvdskoptions').style.display != '') {
+					document.getElementById('skysmartsrvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+		 }
+		 
+		 		 	document.getElementById('optionBillingQA').onclick = function() {
+				if (document.getElementById('billingqasrvdskoptions').style.display != '') {
+					document.getElementById('billingqasrvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+		 }
+		 
+		 		 	document.getElementById('optionOnboarding').onclick = function() {
+				if (document.getElementById('c1srvdskoptions').style.display != '') {
+					document.getElementById('c1srvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+		 }
+		 
+					document.getElementById('optionSchedule').onclick = function() {
+				if (document.getElementById('schedulesrvdskoptions').style.display != '') {
+					document.getElementById('schedulesrvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+		 }
+		 
+		 			document.getElementById('optionTelephony').onclick = function() {
+				if (document.getElementById('telephonysrvdskoptions').style.display != '') {
+					document.getElementById('telephonysrvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+		 }
+		 
+		 		 	document.getElementById('optionAuth').onclick = function() {
+				if (document.getElementById('authsrvdskoptions').style.display != '') {
+					document.getElementById('authsrvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+		 }
+		 
+		 		 	document.getElementById('optionCRM2').onclick = function() {
+				if (document.getElementById('crm2srvdskoptions').style.display != '') {
+					document.getElementById('crm2srvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+		 }
+		 
+					document.getElementById('optionMrkt').onclick = function() {
+				if (document.getElementById('mrktsrvdskoptions').style.display != '') {
+					document.getElementById('mrktsrvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+		 }
+		 
+		 			document.getElementById('optionBilling').onclick = function() {
+				if (document.getElementById('billingsrvdskoptions').style.display != '') {
+					document.getElementById('billingsrvdskoptions').style.display = '';
+					document.getElementById('teacherssrvdskoptions').style.display = 'none';
+					document.getElementById('skysmartsrvdskoptions').style.display = 'none';
+					document.getElementById('billingqasrvdskoptions').style.display = 'none';
+					document.getElementById('c1srvdskoptions').style.display = 'none';
+					document.getElementById('schedulesrvdskoptions').style.display = 'none';
+					document.getElementById('telephonysrvdskoptions').style.display = 'none';
+					document.getElementById('authsrvdskoptions').style.display = 'none';
+					document.getElementById('crm2srvdskoptions').style.display = 'none';
+					document.getElementById('mrktsrvdskoptions').style.display = 'none';
+				} else
+					document.getElementById('billingsrvdskoptions').style.display = 'none';
+		 }	 
+    }
