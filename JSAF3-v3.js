b6edc15243251e4bb1be4c1d919af816c43f0dd1
dimentorexpo <dimentor.expo11@gmail.com>
2022-02-01 07:52:33 +0200

    1

--- JSAF3-v3.js
+++ JSAF3-v3.js
@@ -5293,7 +5293,7 @@ function timerHideButtons() {
                     document.getElementsByClassName('ant-modal-content')[0].children[2].children[i].style.display = 'none'
 
         if (document.getElementsByClassName('ant-modal-content')[0].children[1].children[0].childNodes[0].textContent == 'Создать задачу') { // обращение к функции подсветки и добавления заметки
-            let selectorList = document.querySelectorAll('.sc-fzpans');
+            let selectorList = document.querySelectorAll('.sc-fznKkj');
             if (selectorList.length > 5) {
                 for (let i = 0; i < selectorList.length; i++) {
                     if (selectorList[i].innerText == "Техподдержка исход crm2")
