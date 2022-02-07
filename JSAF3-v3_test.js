152c7b449656851244b4003a1fa72d27a465eb93
grumstv <grumstv@gmail.com>
2022-02-07 14:27:20 +0300

    тестим опять переключатель

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -27,6 +27,61 @@ function mystyles() {
 	.activebtnsd {
 		background-color: #ff6347;
 	}
+	
+		.checkbox-audio {
+			display: inline-block;    
+			height: 28px;    
+			line-height: 28px;  
+			margin-right: 10px;      
+			position: relative;
+			vertical-align: middle;
+			font-size: 14px;
+			user-select: none;	
+		}
+		.checkbox-audio .checkbox-audio-switch {
+			position: relative;	
+			display: inline-block;
+			box-sizing: border-box;			
+			width: 56px;	
+			height: 28px;
+			border: 1px solid rgba(0, 0, 0, .1);
+			border-radius: 25%/50%;	
+			vertical-align: top;
+			background: #eee;
+			transition: .2s;
+		}
+		.checkbox-audio .checkbox-audio-switch:before {
+			content: '';
+			position: absolute;
+			top: 1px;
+			left: 1px;	
+			display: inline-block;
+			width: 24px;	
+			height: 24px;
+			border-radius: 50%;
+			background: white;
+			box-shadow: 0 3px 5px rgba(0, 0, 0, .3);
+			transition: .15s;
+		}
+		.checkbox-audio input[type=checkbox] {
+			display: block;	
+			width: 0;
+			height: 0;	
+			position: absolute;
+			z-index: -1;
+			opacity: 0;
+		}
+		.checkbox-audio input[type=checkbox]:not(:disabled):active + .checkbox-audio-switch:before {
+			box-shadow: inset 0 0 2px rgba(0, 0, 0, .3);
+		}
+		.checkbox-audio input[type=checkbox]:checked + .checkbox-audio-switch {
+			background: limegreen;
+		}
+		.checkbox-audio input[type=checkbox]:checked + .checkbox-audio-switch:before {
+			transform:translateX(28px);
+		}
+	
+	
 	.switch-btn {
 		display: inline-block;
 		width: 62px; /* ширина переключателя */
@@ -101,6 +156,12 @@ var win_AFhelper =  // описание элементов главного ок
 				<button title="Сохраняет ссылки на новый источник звука для входящего запроса в АФ" id="sound_save">save</button> 
 				<button title="Проверка звука при добавленной ссылке" id="sound_test">test</button>
 				<button title="Включение и отключение звука в АФ входящих запросов" id="switcher">ВКЛ</button>
+				
+				<label class="checkbox-audio">
+					<input type="checkbox" checked>
+						<span class="checkbox-audio-switch"></span>
+				</label>
+				
 				<label style="color:bisque"><input type="checkbox" id="removeinfowindow"/>Убрать окно с Info</label>
 				<br>
 				<input title="Ввод часа от 0 до 23 для будильника"" id="setchas" placeholder="HH" autocomplete="off" oninput="maxLengthCheck(this)" type="number" maxlength="2" min="0" max="23" style="text-align: center; margin-top: 5px; width: 50px; color: black;"> <span style="color: white; margin-top: 5px;">:</span>
@@ -4034,6 +4095,7 @@ function move_again_AF() {
         else
             document.getElementById('switcher').innerHTML = 'ВКЛ';
     }
+	
     addInfoUser.style.textAlign = "center"
     addInfoUser.style.color = "white"
     addInfoUser.style = "color: white; text-align: center; cursor: -webkit-grab;"
@@ -4873,6 +4935,7 @@ function startTimer() {
             if (txt != "Взять запрос (0)")
                 audio.play()
         }
+		
 
     if (window.location.href.indexOf('skyeng.autofaq.ai/tickets/assigned') !== -1 && document.getElementsByClassName('expert-user_details-list')[1] !== undefined) {
         vertical = user = ""
@@ -5326,7 +5389,7 @@ function timerHideButtons() {
                     if (selectorList[i].innerText == "Техподдержка 2-я линия crm2")
                         selectorList[i].style.backgroundColor = 'green'
                 }
-            }
+            }			
         }
     }
 }
@@ -7032,6 +7095,8 @@ function toUTF8Array(str) {
     }
     return utf8;
 }
+
+
 
 function decToHex(dec) {
     var hexStr = '0123456789ABCDEF';
