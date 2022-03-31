e60dc1ba161b473401333ea9ef72cdd16a739c4f
grumstv <grumstv@gmail.com>
2022-03-31 11:25:26 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -6812,7 +6812,7 @@ async function checkkcpower() {
 		let found =[];
 	    let str = document.createElement('p')
 		str.style.paddingLeft = '50px' 
-		if (document.getElementById('buttonKCpower').textContent == 'Повторить проверку')
+		if (document.getElementById('buttonTPpower').textContent == 'Повторить проверку' || document.getElementById('buttonKCpower').textContent == 'Повторить проверку')
         document.getElementById('root').children[0].children[1].children[0].children[1].lastElementChild.lastElementChild.remove()
 	
 		await fetch("https://skyeng.autofaq.ai/api/operators/statistic/currentState", {
@@ -6859,7 +6859,7 @@ async function checktppower() {
 		let found =[];
 	    let str = document.createElement('p')
 		str.style.paddingLeft = '50px' 
-		if (document.getElementById('buttonTPpower').textContent == 'Повторить проверку')
+		if (document.getElementById('buttonTPpower').textContent == 'Повторить проверку' || document.getElementById('buttonKCpower').textContent == 'Повторить проверку')
         document.getElementById('root').children[0].children[1].children[0].children[1].lastElementChild.lastElementChild.remove()
 	
 		await fetch("https://skyeng.autofaq.ai/api/operators/statistic/currentState", {
