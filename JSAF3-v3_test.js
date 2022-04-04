1c9cf9d7a282a560ae37dff9ac073f13365eafec
grumstv <grumstv@gmail.com>
2022-04-04 13:35:50 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -378,10 +378,10 @@ var win_Stat =  // описание элементов окна ссылок
 </div>`;
 
 var win_GrList =  // описание элементов окна ссылок
-    `<div style="display: flex; width: 450px;">
-        <span style="width: 450px">
+    `<div style="display: flex; width: 550px;">
+        <span style="width: 550px">
                 <span style="cursor: -webkit-grab;">
-                        <div style="margin: 5px; width: 450;" id="abovebar">
+                        <div style="margin: 5px; width: 400;" id="statdata">
                                 <button id="hideMeGrList" style="width:50px; background: #228B22;">hide</button>
                         </div>
 						
