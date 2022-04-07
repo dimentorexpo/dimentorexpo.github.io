4c14903d744d5599ce99bf512d3d39c32c900969
grumstv <grumstv@gmail.com>
2022-04-07 16:53:02 +0300

    disable Walentine

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -7672,7 +7672,7 @@ function prepTp() {
         include("https://dimentorexpo.github.io/MobilePass.js") // модуль генерации одноразового пароля для моб приложения
         include("https://dimentorexpo.github.io/ServiceDesk.js")
         include("https://code.jquery.com/jquery-3.6.0.js") // подключаем модуль обработки JQuery
-        include("https://dimentorexpo.github.io/viewSlack.js") // подключаем модуль Баг-репорта валентина
+       // include("https://dimentorexpo.github.io/viewSlack.js") // подключаем модуль Баг-репорта валентина
         include("https://dimentorexpo.github.io/unsub.js") // подключаем модуль unsub валентина
     }, 2000)
 
