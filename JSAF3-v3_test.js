90288a2fe19efcc0c5ca309ff9244a165b357662
grumstv <grumstv@gmail.com>
2022-04-14 13:07:59 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1662,7 +1662,7 @@ var chatsArray = []
 var TS_addr = 'https://script.google.com/macros/s/AKfycbyuK-HoVzF2v66klEcqNyAKFFqtvVheEe4vLhRz/exec'
 var KC_addr = 'https://script.google.com/macros/s/AKfycbzNJgvbbgMIRzEuIMv2yR2VRE5lT7xrhouGVod0/exec'
 var TP_addr = 'https://script.google.com/macros/s/AKfycbydMLmE-OOY2MMshHopMe0prA5lS0CkaR7-rQ4p/exec'
-var TP_addr2 = 'https://script.google.com/macros/s/AKfycbxARCreNyVYkn4feuYIzbhGpEbmr7cXPBIQYogp9GAdabA38FsV/exec'
+var TP_addr2 = 'https://script.google.com/macros/s/AKfycbwgmUqogmYH_mRQOFelhPvmk9xCabowzLjg9li1Sg-_jJ_Zs1LKvbzWwVhhLL5mkhY3sg/exec'
 var flagLangBut = 0
 function move_again_AF() {
 
