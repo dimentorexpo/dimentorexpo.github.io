a372311e3c9e41038d3a53f1ace1ee8882bee86c
grumstv <grumstv@gmail.com>
2022-04-26 11:37:52 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -6342,7 +6342,7 @@ function checJiraF() {
         }
 
         document.querySelector("#DateFilter > div:nth-child(3) > div > div > div > div > span > span.sc-fzqNqU") .onclick = function () {
-            if (document.querySelector("#DateFilter > div:nth-child(2) > div > div > div > div > span > span.sc-fzqNqU jlJPWV content") .innerText != "Пусто") {
+            if (document.querySelector("#DateFilter > div:nth-child(3) > div > div > div > div > span > span.sc-fzqNqU jlJPWV") .innerText != "Пусто") {
                 sendComment(document.querySelector("#DateFilter > div:nth-child(3) > div > div > div > div > span > span.sc-fzqNqU").innerText);
                 console.log("DONE!")
             }
