7345d6b5b59ce4d8d631bf7bb75c8f8b53fd3710
grumstv <grumstv@gmail.com>
2022-05-02 12:41:50 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1396,13 +1396,7 @@ marksstata.onclick = async function() {
 
                 await fetch("https://skyeng.autofaq.ai/api/conversations/history", {
                     "headers": {
-                        "accept": "*/*",
-                        "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
                         "content-type": "application/json",
-                        "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"",
-                        "sec-ch-ua-mobile": "?0",
-                        "sec-ch-ua-platform": "\"Windows\"",
-                        "sec-fetch-dest": "empty",
                         "sec-fetch-mode": "cors",
                         "sec-fetch-site": "same-origin"
                     },
