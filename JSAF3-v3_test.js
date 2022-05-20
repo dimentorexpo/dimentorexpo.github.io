ac4f86aaf1efd4f3c23bb38cb4d5654cbcef91d5
grumstv <grumstv@gmail.com>
2022-05-21 01:13:48 +0300

    lit fixes

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -899,21 +899,20 @@ var win_servicedesk =
 				</div>
 
                 <div id="studcaboptions" style="display: none; margin-left:20px;">
-                <p style="color:bisque;font-size:18px;position:relative; top:7px; left:10px;">#student-cabinet-bugs; Сообщаем о проблемах во взрослом и детском ЛКУ (страницы на домене student.skyeng.ru </p>
-                <button class="studcabbtn" id="studadultcab">Взрослый ЛКУ Главная страница </button>
-                <button class="studcabbtn" id="studkidcab">Детский ЛКУ Главная страница </button>
+                <p style="color:bisque;font-size:18px;position:relative; top:7px; left:10px;">#student-cabinet-bugs; Сообщаем о проблемах во взрослом и детском ЛКУ (страницы на домене student.skyeng.ru)</p>
+                <button class="studcabbtn" id="studadultcab">Взрослый ЛКУ Главная страница</button>
+                <button class="studcabbtn" id="studkidcab">Детский ЛКУ Главная страница</button>
+			    <button class="studcabbtn" id="studcabfamandcourse">Страница семьи и курсов</button>
                 <button class="studcabbtn" id="studstories">Stories </button>
                 <button class="studcabbtn" id="studcabrefpage">Реферальная страница</button>
-                <button class="studcabbtn" id="studcabtransfpayhist">Страница оплаты, трансфера и истории баланса </button>
-                <button class="studcabbtn" id="studcabttmovelesson">Страница расписания и переноса урока </button>
-                <button class="studcabbtn" id="studcabteacherpage">Страница преподавателя </button>
-                <button class="studcabbtn" id="studcabprofilesettings">Страница профиля У настройки </button>
-                <button class="studcabbtn" id="studcabfamandcourse">Страница семьи и курсов </button>
-                <button class="studcabbtn" id="studcabshowcase">Страница шоукейса (подключение услуг) </button>
-                <button class="studcabbtn" id="studcabmenunav">Меню навигации (лейаут) </button>
-                <button class="studcabbtn" id="studcabunderground">Подземный стук </button>
+                <button class="studcabbtn" id="studcabtransfpayhist">Страница оплаты, трансфера и истории баланса</button>
+                <button class="studcabbtn" id="studcabttmovelesson">Страница расписания и переноса урока</button>
+                <button class="studcabbtn" id="studcabteacherpage">Страница преподавателя</button>
+                <button class="studcabbtn" id="studcabprofilesettings">Страница профиля У настройки</button>
+				<button class="studcabbtn" id="studcabmenunav">Меню навигации (лейаут) </button>
+                <button class="studcabbtn" id="studcabshowcase">Страница шоукейса (подключение услуг)</button>
+                <button class="studcabbtn" id="studcabunderground">Подземный стук</button>
                 </div>
-
 				
 	        </span>
 						
