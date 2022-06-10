be1debda51f23fbdfa3ba8d2a34f420c5cc360e8
grumstv <grumstv@gmail.com>
2022-06-10 21:52:33 +0300

    Пробуем отрефакторить код функции поиска оценок

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -1600,7 +1600,7 @@ buttonhistory.onclick = function () { //функция приска пр ист�
                 document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0]
         }
         search.click()
-    } else if (document.querySelector('#hide_or_display').textContent == "свернуть") {
+    } else {
         for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
             if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
                 document.getElementById('user_id').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0]
@@ -1610,11 +1610,8 @@ buttonhistory.onclick = function () { //функция приска пр ист�
 }
 
 marksstata.onclick = async function () {
-
-    if (document.getElementById('AF_Marks').style.display == 'none') {
-        document.getElementById('AF_Marks').style.display = ''
-
-        var date = new Date()
+	
+	        var date = new Date()
 
         day = month = ""
         if (date.getMonth() < 9)
@@ -1640,15 +1637,33 @@ marksstata.onclick = async function () {
 
         secondDate = date.getFullYear() + "-" + month + "-" + day + "T" + hours + ":" + minutes + ":" + seconds + ".000z"
 
+    if (document.getElementById('AF_Marks').style.display == 'none') {
+        document.getElementById('AF_Marks').style.display = ''
+
         for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
             if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
                 document.getElementById('useridsearch').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0];
         }
         let tempval = document.getElementById('useridsearch').value;
         findmarksstat.click();
-        document.getElementById('markstable').innerText = "Загрузка..."
+		findMarks();
 
-        await fetch("https://skyeng.autofaq.ai/api/conversations/history", {
+    } else {
+        document.getElementById('findmarksstat').onclick = async function () {
+            let tempval = document.getElementById('useridsearch').value.trim();
+
+			findMarks();
+        }
+    }
+
+    document.getElementById('clearmarksstat').onclick = function () {
+        document.getElementById('markstable').innerHTML = "";
+    }
+}
+
+async function findMarks() {
+	document.getElementById('markstable').innerText = "Загрузка..."
+	await fetch("https://skyeng.autofaq.ai/api/conversations/history", {
             "headers": {
                 "content-type": "application/json",
                 "sec-fetch-mode": "cors",
@@ -1697,99 +1712,6 @@ marksstata.onclick = async function () {
             'Закрыто без оценок: ' + clswoutmark + ' ............. ' + (clswoutmark / datamarks.total * 100).toFixed(1) + '%' + '<br>' +
             'Автозакрытие: ' + (datamarks.total - clswoutmark - markscount) + ' ....................... ' + ((datamarks.total - clswoutmark - markscount) / datamarks.total * 100).toFixed(1) + '%';
         document.getElementById('useridsearch').value = "";
-
-    } else {
-        document.getElementById('findmarksstat').onclick = async function () {
-            let tempval = document.getElementById('useridsearch').value.trim();
-            document.getElementById('markstable').innerText = "Загрузка..."
-
-            var date = new Date()
-
-            day = month = ""
-            if (date.getMonth() < 9)
-                month = "0" + (date.getMonth() + 1)
-            else
-                month = (date.getMonth() + 1)
-            if (date.getDate() < 10)
-                day = "0" + date.getDate()
-            else
-                day = date.getDate()
-            if (date.getHours() < 10)
-                hours = '0' + date.getHours()
-            else
-                hours = date.getHours()
-            if (date.getMinutes() < 10)
-                minutes = '0' + date.getMinutes()
-            else
-                minutes = date.getMinutes()
-            if (date.getSeconds() < 10)
-                seconds = '0' + date.getSeconds()
-            else
-                seconds = date.getSeconds()
-
-            secondDate = date.getFullYear() + "-" + month + "-" + day + "T" + hours + ":" + minutes + ":" + seconds + ".000z"
-
-            await fetch("https://skyeng.autofaq.ai/api/conversations/history", {
-                "headers": {
-                    "accept": "*/*",
-                    "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-                    "content-type": "application/json",
-                    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"",
-                    "sec-ch-ua-mobile": "?0",
-                    "sec-ch-ua-platform": "\"Windows\"",
-                    "sec-fetch-dest": "empty",
-                    "sec-fetch-mode": "cors",
-                    "sec-fetch-site": "same-origin"
-                },
-                "referrer": "https://skyeng.autofaq.ai/tickets/archive",
-                "referrerPolicy": "strict-origin-when-cross-origin",
-                "body": "{\"serviceId\":\"361c681b-340a-4e47-9342-c7309e27e7b5\",\"mode\":\"Json\",\"channelUserFullTextLike\":\"" + tempval + "\",\"tsFrom\":\"2022-01-01T00:00:00.000Z\",\"tsTo\":\"" + secondDate + "\",\"orderBy\":\"ts\",\"orderDirection\":\"Desc\",\"page\":1,\"limit\":100}",
-                "method": "POST",
-                "mode": "cors",
-                "credentials": "include"
-            }).then(r => r.json()).then(r => datamarks = r)
-
-            let count = {};
-            let clswoutmark = 0;
-            let markscount = 0;
-            let flagok = [];
-            for (let i = 0; i < datamarks.items.length; i++) {
-                if (datamarks.items[i].stats.rate != undefined && datamarks.items[i].stats.rate.rate == undefined)
-                    clswoutmark++;
-                if (datamarks.items[i].stats.rate != undefined && datamarks.items[i].stats.rate.rate != undefined)
-                    flagok.push(datamarks.items[i].stats.rate.rate)
-            }
-            flagok.forEach(function (i) { count[i] = (count[i] || 0) + 1; });
-            console.log(count);
-            if (count[1] == undefined)
-                count[1] = 0;
-            if (count[2] == undefined)
-                count[2] = 0;
-            if (count[3] == undefined)
-                count[3] = 0;
-            if (count[4] == undefined)
-                count[4] = 0;
-            if (count[5] == undefined)
-                count[5] = 0;
-            markscount = (count[1] + count[2] + count[3] + count[4] + count[5]);
-            document.getElementById('markstable').innerHTML = 'Пользователь 🕵️‍♀️: ' + tempval + '<br>' +
-                'Name: ' + datamarks.items[0].channelUser.fullName + '<br>' +
-                'Оценка 1 🤬: ' + count[1] + ' ................... ' + ((count[1] / markscount) * 100).toFixed(1) + '%' + '<br>' +
-                'Оценка 2 🤢: ' + count[2] + ' ................... ' + ((count[2] / markscount) * 100).toFixed(1) + "%" + '<br>' +
-                'Оценка 3 😐: ' + count[3] + ' ................... ' + ((count[3] / markscount) * 100).toFixed(1) + "%" + '<br>' +
-                'Оценка 4 🥴: ' + count[4] + ' ................... ' + ((count[4] / markscount) * 100).toFixed(1) + "%" + '<br>' +
-                'Оценка 5 😊: ' + count[5] + ' ................... ' + ((count[5] / markscount) * 100).toFixed(1) + '%' + '<br>' +
-                'Всего оценок: ' + markscount + '<br>' + 'Обращений с начала года: ' + datamarks.total + '<br>' +
-                'Оценки/кол-во обращений: ' + ((markscount / datamarks.total) * 100).toFixed(1) + '%' + '<br>' +
-                'Закрыто без оценок: ' + clswoutmark + ' ............. ' + (clswoutmark / datamarks.total * 100).toFixed(1) + '%' + '<br>' +
-                'Автозакрытие: ' + (datamarks.total - clswoutmark - markscount) + ' ....................... ' + ((datamarks.total - clswoutmark - markscount) / datamarks.total * 100).toFixed(1) + '%';
-            document.getElementById('useridsearch').value = "";
-        }
-    }
-
-    document.getElementById('clearmarksstat').onclick = function () {
-        document.getElementById('markstable').innerHTML = "";
-    }
 }
 
 buttonnextstudentid.onclick = function () {
