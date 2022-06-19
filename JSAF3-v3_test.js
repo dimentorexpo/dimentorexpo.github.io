361f29940bd35a34a2fc37781092b0c90a0ca870
grumstv <grumstv@gmail.com>
2022-06-19 20:59:10 +0300

    УРА РАБОТАЕТ ДЛЯ ХЕША ЧАТА

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -5683,24 +5683,7 @@ function move_again_AF() {
                 for (let i = 0; i < convdata.messages.length; i++) {
                     timearr.push(new Date(convdata.messages[i].ts).toLocaleDateString('ru-RU', options))
                     switch (convdata.messages[i].tpe) {
-                        case "Question":
-						// testarray = convdata.messages[i].txt.match(/<p>(.*?)<\/p>/gm);
-						// console.log(convdata.messages[i].txt.match(/<p>(.*?)<\/p>/gm))
-						
-						// if (testarray !=null) {
-							// temppics = [];
-						// for (let i=0; i< testarray.length; i++) {
-							// if (testarray[i].match(/https:\/\/vimbox-resource.*jpg/gm) !=null)
-							   // temppics.push(testarray[i].match(/https:\/\/vimbox-resource.*jpg/gm)[0])
-							// else if (testarray[i].match(/https:\/\/vimbox-resource.*png/gm) !=null)
-								// temppics.push(testarray[i].match(/https:\/\/vimbox-resource.*png/gm)[0])
-							// else if (testarray[i].match(/https:\/\/vimbox-resource.*jpeg/gm) !=null)         
-								// temppics.push(testarray[i].match(/https:\/\/vimbox-resource.*jpeg/gm)[0]) 
-						// }
-						
-						// console.log(temppics)
-						// }
-						
+                        case "Question":				
                             if (convdata.messages[i].click == undefined) {
 								
 						testarray = convdata.messages[i].txt.match(/<p>(.*?)<\/p>/gm);
