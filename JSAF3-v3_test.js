f3dea9161ce530f9b962eec5f2413b8d7400cbb6
grumstv <grumstv@gmail.com>
2022-06-21 23:35:32 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -642,7 +642,7 @@ var win_Chathis =  // описание элементов окна ссылок
 				<div style="margin: 5px; width: 550;" id="chathisheader">
 					<button title="Скрытие меню" id="hideMeChHis" style="width:50px; background: #228B22;">hide</button>
 					<button title="Очистка всех полей" id="clearallinfo" style="width:50px;">🧹</button>
-					<select style="height:28px; width:270px; text-align:center" id="operatorstp" onchange=`findchatsoper${this}`>
+					<select style="height:28px; width:270px; text-align:center" id="operatorstp" onchange="findchatsoper()">
 							<option selected="" disabled="">Операторы на линии</option>
 					</select>
 					<button title="Ищет по выбранному оператору активные чаты, чтобы можно было их просмотреть" id="FindChatsOnOperator" style="width:50px;">🚀</button>
