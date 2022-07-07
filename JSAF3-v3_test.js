41fb1a13e5db69691fb5e4f4098ad74728adb5c7
grumstv <grumstv@gmail.com>
2022-07-07 20:02:23 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -415,7 +415,7 @@ var win_AFhelper =  // описание элементов главного ок
 				<button id="opandclsbarhyper" style="width:30px; margin:0; padding:2px; text-align:center;">🔗</button>
                 <button title="Отправить текст от имени бота" id="sndbot" style="width:30px; margin-left:5px">🤖</button>
 				<button title="Отправить текст" id="snd" style="width:50px; margin-left:5px">send</button>
-				<button title="Переключает между отправкой текста в заметки или в чат пользователю" id="msg" style="width:80px; margin-left:25px">Чат</button>
+				<button title="Переключает между отправкой текста в заметки или в чат пользователю" id="msg" style="width:80px; margin-left:20px">Чат</button>
 			</div>
 		<div style="border: 2px double black; display: none; background-color: #464451" id="addTmp">
 			<div style="margin: 5px; width: 350px">
