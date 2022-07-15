ce94fc2911740b6e374429eac69748186e7598aa
grumstv <grumstv@gmail.com>
2022-07-16 00:01:01 +0300

    clwrk

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -9618,7 +9618,7 @@ function dosetclasswork(subject) {
     let classworkbtn = document.createElement('div')
     classworkbtn.id = "clwbtn"
     classworkbtn.innerText = "Classwork"
-    classworkbtn.style = "position:absolute; top:14px; left:65%; cursor: pointer;"
+    classworkbtn.style = "position:absolute; top:14px; left:55.5%; cursor: pointer;"
     let subject = document.URL.split('/')[4] + "/" + document.URL.split('/')[5]
 
     switch (subject) {
