826e8e8bd0fd7414e10d2fa0e3f7aeda19a66cc9
grumstv <grumstv@gmail.com>
2022-08-17 14:27:16 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -7827,7 +7827,24 @@ function msgFromTable(btnName) { //шаблоны, тематики. теги с
 				
                 break
             } else {
-                if (table[l][4] == "") {
+				
+				if(table[l][1].indexOf("Рандом") != -1) {
+					var counttmpleng = table[l][1][9]
+					var newLeng = Math.floor(Math.random() * (counttmpleng))
+					let splittedarreng = table[l][5+newLeng].split('$')
+					console.log(splittedarreng)
+					if (splittedarreng[0] == "Текст")
+						sendAnswer(splittedarreng[1])
+					else if (splittedarreng[0] == "Шаблон") {
+						sendAnswerTemplate(splittedarreng[1], splittedarreng[1])
+					} else {
+						document.getElementById('inp').value = "Нет такого шаблона"
+					}
+
+					//sendAnswer(table[newL][2])
+				}
+				
+                else if (table[l][4] == "") {
                     document.getElementById('inp').value = "Нет такого шаблона"
                 } else {
                     if (table[l][5] == "Быстрый шаблон") {
