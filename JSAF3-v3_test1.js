5a4e03d035c475ab6b1d5f64d9b40aac926d1d6e
dimentorexpo <dimentor.expo11@gmail.com>
2022-08-25 09:10:51 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -3021,8 +3021,8 @@ wintCreateTask.onmouseup = function () { document.removeEventListener('mousemove
 
 function noinputerror (nameofelem){ // мигаем полем где не введено обязательное значение
     for (let i = 1; i < 4; i++){
-        nameofelem.style.border = '3px solid red';
-        setTimeout(function () {nameofelem.style.border = ''}, 1000)
+        setTimeout(function () {nameofelem.style.border = '3px solid red'}, 500);
+        setTimeout(function () {nameofelem.style.border = ''}, 500);
     }
 }
 
