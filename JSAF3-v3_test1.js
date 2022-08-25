28b8cc2e48480928557174f08e8504de4087a83e
dimentorexpo <dimentor.expo11@gmail.com>
2022-08-25 10:48:11 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -3020,10 +3020,12 @@ wintCreateTask.onmousedown = function (a) {
 wintCreateTask.onmouseup = function () { document.removeEventListener('mousemove', listenerTaskCreate); }
 
 function noinputerror (nameofelem){ // мигаем полем где не введено обязательное значение
-    for (let i = 1; i < 4; i++){
-        setTimeout(function () {document.getElementById(nameofelem).style.border = '3px solid red'}, 1000);
-        setTimeout(function () {document.getElementById(nameofelem).style.border = ''}, 1000);
-    }
+    if (nameofelem != '') nameofelem = '';
+        else nameofelem = '3px solid red';
+//    for (let i = 1; i < 4; i++){
+//        setTimeout(function () {document.getElementById(nameofelem).style.border = '3px solid red'}, 1000);
+//        setTimeout(function () {document.getElementById(nameofelem).style.border = ''}, 1000);
+//    }
 }
 
 function checkelementtype (a){ // проверка на какой элемент нажали
@@ -6513,11 +6515,12 @@ function move_again_AF() { //с АФ шняга там стили шмили с�
 
                 if (document.getElementById('chatlnk').value.length < 3){
                     //document.getElementById('chatlnk').style.backgroundColor = 'red';
-                    nameofelem = this.id;
-                    noinputerror(nameofelem);
-                    flagempty = 1;    
-                    //} else {
+                    nameofelem = document.getElementById('chatlnk').style.border;
+                    flagempty = 1;
+                    var chatlnkinterval = setInterval(noinputerror(nameofelem), 500);
+                } else {
                     //document.getElementById('chatlnk').style.backgroundColor = '';
+                    clearInterval(chatlnkinterval);
                     }
                 
                 if (document.getElementById('userissue').children[0].selected == true){
