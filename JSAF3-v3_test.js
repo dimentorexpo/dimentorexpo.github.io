672776463a4c7a9bb5afc131a1c954a22973f57f
grumstv <grumstv@gmail.com>
2022-09-01 17:45:49 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -6192,7 +6192,7 @@ document.getElementById('JiraOpenForm').onclick = function() { // открыва
 
                     if (rezissuetable.issueTable.issueKeys[i] != undefined) {
 						
-						if (rezissuetable.issueTable.table.match(/(\w+-\d+">.*?).<\/a>/gmi).filter(function (item, index, array) { if (index % 2 != 0) return item; })[i].replace('">',' – ').indexOf(document.getElementById('testJira').value) !=-1) {
+						if (rezissuetable.issueTable.table.match(/(\w+-\d+">.*?).<\/a>/gmi).filter(function (item, index, array) { if (index % 2 != 0) return item; })[i].replace('">',' – ').value.toLowerCase().indexOf(document.getElementById('testJira').value.toLowerCase()) !=-1) {
 							temporarka = rezissuetable.issueTable.table.match(/(\w+-\d+">.*?).<\/a>/gmi).filter(function (item, index, array) { if (index % 2 != 0) return item; })[i].replace('">',' – ').replace(new RegExp(document.getElementById('testJira').value,'i'), `<span style="color:lightgreen; font-weight:700;">${document.getElementById('testJira').value}</span>`)
 						} else {
 							temporarka = rezissuetable.issueTable.table.match(/(\w+-\d+">.*?).<\/a>/gmi).filter(function (item, index, array) { if (index % 2 != 0) return item; })[i].replace('">',' – ')
