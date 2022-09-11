8390e87513cf582bcc863f07b29b5950adb17bd7
grumstv <grumstv@gmail.com>
2022-09-11 21:36:40 +0300

    Update JSAF3-v3_test.js

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -6688,13 +6688,8 @@ function move_again_AF() { //с АФ шняга там стили шмили с�
 				let arroffavbugs = document.getElementsByName('removefromfavourites');
 				for (let i = 0; i<arroffavbugs.length; i++) {
 					arroffavbugs[i].onclick = function () {
-								delete arroffavbugs[i]
+								favissues.splice([i],1)
 								localStorage.setItem('bugsarray', JSON.stringify(favissues))
-								favissues =  JSON.parse(localStorage.getItem('bugsarray'))
-								document.getElementById('favouriteissuetable').innerHTML = favissues;
-
-								// favissues.splice([i],1)
-								// localStorage.setItem('bugsarray', JSON.stringify(favissues))
 								// favissues =  JSON.parse(localStorage.getItem('bugsarray'))
 								// document.getElementById('favouriteissuetable').innerHTML = favissues;
 					}
