eb48986d86465a153f708829f1f27da9288f40df
dimentorexpo <dimentor.expo11@gmail.com>
2022-09-17 15:52:21 +0300

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -9222,7 +9222,7 @@ function startTimer() {
         if (window.location.href.indexOf('skyeng.autofaq.ai/tickets/assigned') !== -1) {
             txt = document.getElementsByClassName('expert-sidebar-button')[0].childNodes[1].childNodes[0].innerHTML
             if (txt[14] > 0)
-                audio.play()
+                setTimeout(() => {audio.play()},3000)
         }
 
 
