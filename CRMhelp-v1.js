4f1c7b39c069e10733d5c184fcf83754a5091883
grumstv <grumstv@gmail.com>
2022-11-02 11:31:23 +0300

    Update CRMhelp-v1.js

--- CRMhelp-v1.js
+++ CRMhelp-v1.js
@@ -1,4 +1,28 @@
+let upmenubtn = document.createElement('span')
+upmenubtn.innerText = "Меню"
+upmenubtn.id = 'MenubarCRM'
+upmenubtn.style="cursor:pointer;font-weight:500; text-shadow: 1px 0 1px #000, 0 1px 1px #000, -1px 0 1px #000, 0 -1px 1px #000;"
 
-	function test() {
-		console.log('YRA ZARABOTALO')
-	}
+document.getElementsByClassName('mat-toolbar-row')[0].children[1].children[0].append(upmenubtn)
+
+
+let menubarcrm = document.createElement('div')
+menubarcrm.style = `background: white; position:absolute; left: 950px; top: 50px; border: 0px solid #000000; display:none; min-height: 60px; min-width:150px; box-shadow: -1px 4px 16px 7px rgba(34, 60, 80, 0.09); z-index:999;`
+menubarcrm.id = 'idmymenucrm'
+
+document.body.append(menubarcrm)
+
+document.getElementById('MenubarCRM').onclick = function() {
+    if (document.getElementById('idmymenucrm').style.display == 'none')  {
+        document.getElementById('idmymenucrm').style.display =''
+    } else {
+        document.getElementById('idmymenucrm').style.display ='none'
+
+    }
+
+}
+
+let jirasearchbtn = document.createElement('div')
+jirasearchbtn.innerText = "🔎Jira search"
+jirasearchbtn.style="cursor:pointer"
+document.getElementById('idmymenucrm').append(jirasearchbtn)
