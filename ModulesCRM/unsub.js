57200b03ff2bffb3988261d03e31c72cb60b2f09
grumstv <grumstv@gmail.com>
2022-11-02 18:09:12 +0300

    Update unsub.js

--- ModulesCRM/unsub.js
+++ ModulesCRM/unsub.js
@@ -281,8 +281,7 @@ buttonOpenForm.onclick = function() {
 		createSlackView()
 	this.style.display = 'none'
 }
-var btnAdd = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
-btnAdd.insertBefore(buttonOpenForm, btnAdd.children[0])
+
 function submitSlackView(view) {
 	console.log(view)
 	let client_token = Number(new Date())
