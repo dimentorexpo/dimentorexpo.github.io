b4987c397a3589ec6deb255caa8187f092c92ff7
grumstv <grumstv@gmail.com>
2022-11-05 15:20:27 +0300

    Update Linksdostup.js

--- ModulesCRM/Linksdostup.js
+++ ModulesCRM/Linksdostup.js
@@ -5,6 +5,10 @@ var win_linksd =  // описание элементов окна доступо
                         <div style="margin: 5px; width: 409px;" id="linksd_1str">
                             <button title="скрывает меню" id="hideMeLinksd" style="width:50px; background: #228B22;">hide</button>
                         </div>
+						<div>
+							<button id="curVeriOSCRM"></button>
+							<button id="curVerAndroidCRM"></button>
+						<div>
                         <div style="margin: 5px; margin-top: 0px; width: 409px" id="linksd_kib_box">
                             <p style="margin-left: 42%; margin-bottom: 0px; margin-top: 0px; color: #F6358A; font-size: 16px">Kibana</p>
                             <input id="kibsvid" placeholder="ID Summary" title="Вводим id пользователя для открытия Video | Tech Summary" autocomplete="off" type="text" style="text-align: center; width: 103px; color: black; margin-top: 5px">
@@ -90,6 +94,19 @@ document.getElementById('AF_Linksd').ondblclick = function (a) { // скрыти
         };
         kibsvid.value = "";
     }
+	
+	if (tableres != null || tableres !=undefined)  {
+		document.getElementById('curVeriOSCRM').textContent  = tableres[1][0] + ':' + tableres[1][1]
+		document.getElementById('curVeriOSCRM').addEventListener('click', function () { // открываем актуальную версию приложения iOS
+		window.open("https://apps.apple.com/ru/app/skyeng-%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%8F%D0%B7%D1%8B%D0%BA-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD/id1065290732")
+		})
+
+		document.getElementById('curVerAndroidCRM').textContent  = tableres[0][0] + ':' + tableres[0][1]
+		document.getElementById('curVerAndroidCRM').addEventListener('click', function () { // открываем актуальную версию приложения Android
+			window.open("https://play.google.com/store/apps/details?id=skyeng.words.prod")
+		})
+	
+	}
 
     document.getElementById('kibsvheshbut').onclick = function () { // kibana Tech Summary - хэш
         if (kibsvhesh.value == "") {
