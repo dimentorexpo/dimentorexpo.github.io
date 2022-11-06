b1997fdf985b15c366631d23919ab4b600b6ca4b
grumstv <grumstv@gmail.com>
2022-11-07 01:52:29 +0300

    Update unsub.js

--- Modules/unsub.js
+++ Modules/unsub.js
@@ -238,7 +238,8 @@ let buttonOpenForm = document.createElement('div');
 buttonOpenForm.id = 'buttonOpenForm';
 buttonOpenForm.textContent = "📧Unsub";
 buttonOpenForm.classList.add('inithide');
-buttonOpenForm.onclick = function() {
+if (buttonOpenForm != null) {
+	buttonOpenForm.onclick = function() {
 	document.getElementById('idmymenu').style.display = 'none'
 	if(socketOpened == 0) {
 		if(localStorage.getItem('token') == undefined)
@@ -250,7 +251,9 @@ buttonOpenForm.onclick = function() {
 	else
 		createSlackView()
 	this.style.display = 'none'
+	}
 }
+
 var btnAdd = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
 btnAdd.insertBefore(buttonOpenForm, btnAdd.children[0])
 function submitSlackView(view) {
