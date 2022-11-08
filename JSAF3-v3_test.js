cfcc5838abc942f619dbe4473654622c4314da57
grumstv <grumstv@gmail.com>
2022-11-08 16:11:51 +0300

    откат

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -291,7 +291,7 @@ function mystyles() {
 			padding-left:5px;
 		}
 		
-		#servDsk:hover, #buttonOpenForm:hover, #butMarks:hover, #suggestform:hover, #otkaz:hover, #butChatHistory:hover, #butLessonInfo:hover, #JiraOpenForm:hover, #smartroomform:hover, #butFrozeChat:hover, #buttonGetStat:hover {
+		#servDsk:hover, #buttonOpenForm:hover, #butMarks:hover, #suggestform:hover, #otkaz:hover, #butChatHistory:hover, #butLessonInfo:hover, #JiraOpenForm:hover, #smartroomform:hover, #butFrozeChat:hover {
 			background:DeepSkyBlue;
 			color:#ffffffe6;
 			font-weight:600;
@@ -408,6 +408,9 @@ function mystyles() {
 	}
 	.inithide {
 		display:none !important;
+	}
+	#buttonGetStat:hover {
+		background:DeepSkyBlue;
 	}
 	
 	`
