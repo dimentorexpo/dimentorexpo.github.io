ac86510fcc3f23d0ec9ba72b2d0c74121ee9c72a
grumstv <grumstv@gmail.com>
2022-11-09 15:51:43 +0300

    Update CRMhelp-v1.js

--- CRMhelp-v1.js
+++ CRMhelp-v1.js
@@ -475,13 +475,13 @@ include("https://dimentorexpo.github.io/Lightbox/dist/js/lightbox.min.js") // п
 		let domParse = new DOMParser()
 		let imagelink = domParse.parseFromString(divimg, `text/html`).querySelectorAll('a')
 		for (let j = 0; j < imagelink.length; j++) {
-			if (imagelink.querySelectorAll('a')[j].hasAttribute('data-lightbox') == false) {
-				var img = document.createElement('img')
-				img.style.width = '100px'
+			if (imagelink[j].hasAttribute('data-lightbox') == false) {
+                var img = document.createElement('img')
+            	img.style.width = '100px'
 				var alink = document.createElement('a')
 				alink.setAttribute('data-lightbox', 'imgs');
 				alink.append(img)
-				img.src = div.querySelectorAll('a')[j].href
+            	img.src = imagelink[j].href
 				img.alt = 'Изображение'
 				alink.href = img.src;
 				imagelink[j].replaceWith(alink)
