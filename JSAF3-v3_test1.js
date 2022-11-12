f952b82cd1f2468c041122bc542f0406c500b9d0
dimentorexpo <dimentor.expo11@gmail.com>
2022-11-12 08:50:48 +0200

    Update JSAF3-v3_test1.js

--- JSAF3-v3_test1.js
+++ JSAF3-v3_test1.js
@@ -1125,7 +1125,7 @@ hashBut.onclick = function () { // кнопка копирующая хеш ча
 function checkelementtype(a) { // проверка на какой элемент нажали
     let elem = document.elementFromPoint(a.clientX, a.clientY)
 
-    if (elem.nodeName != 'BUTTON' && elem.nodeName != 'INPUT' && elem.nodeName != 'TEXTAREA' && elem.nodeName != 'SELECT' && elem.className != 'checkbox-audio-switch') {
+    if (elem.nodeName != 'BUTTON' && elem.nodeName != 'INPUT' && elem.nodeName != 'TEXTAREA' && elem.nodeName != 'SELECT' && elem.className != 'checkbox-audio') {
         return true;
     }
     return false;
