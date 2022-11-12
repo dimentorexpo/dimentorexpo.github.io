0283714cc72c58fee5b094e364d5cee9ddae7e0a
dimentorexpo <dimentor.expo11@gmail.com>
2022-11-12 10:33:32 +0200

    Update SettingsApp.js

--- ModulesCRM/SettingsApp.js
+++ ModulesCRM/SettingsApp.js
@@ -303,7 +303,7 @@ function checkforsoundplay() {
 
 setInterval(checkforsoundplay, 1000);
 */
-document.getElementById("MenubarCRM").addEventListener("DOMSubtreeModified", function () {
+document.getElementById("MenubarCRM").addEventListener('DOMSubtreeModified', function () {
     if (window.location.href.indexOf('https://crm2.skyeng.ru/customer-support/start') !== -1) {
         console.log('Типа того')
     }
