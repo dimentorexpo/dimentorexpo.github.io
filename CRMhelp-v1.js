2bdd961b3842c87df3329f8e916211594a96638a
dimentorexpo <dimentor.expo11@gmail.com>
2022-11-12 15:50:27 +0200

    Update CRMhelp-v1.js

--- CRMhelp-v1.js
+++ CRMhelp-v1.js
@@ -260,7 +260,7 @@ function logginerfortestsCRM(polzovatel) {
 function checkelementtype(a) { // проверка на какой элемент нажали
     let elem = document.elementFromPoint(a.clientX, a.clientY)
 
-    if (elem.nodeName != 'BUTTON' && elem.nodeName != 'INPUT' && elem.nodeName != 'TEXTAREA' && elem.nodeName != 'SELECT' && elem.className != "checkbox-audio-switch") {
+    if (elem.nodeName != 'BUTTON' && elem.nodeName != 'INPUT' && elem.nodeName != 'TEXTAREA' && elem.nodeName != 'SELECT' && elem.className != "checkbox-audio-switch-CRM") {
         return true;
     }
     return false;
