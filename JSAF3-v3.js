5778899d0d24a30f45145b8fc25f4f8bd3bbf932
grumstv <grumstv@gmail.com>
2022-11-23 23:30:54 +0300

    Update JSAF3-v3.js

--- JSAF3-v3.js
+++ JSAF3-v3.js
@@ -672,8 +672,8 @@ async function whoAmI() { // функция получения айди опер
         "credentials": "include"
     }).then(a => b = a.json()).then(b => {
         let me = document.querySelector('.user_menu-dropdown-user_name');
-        operatorsarray = b.rows;
-        b.rows.forEach(s => {
+        operatorsarray = b.onOperator;
+        b.onOperator.forEach(s => {
             if (s.operator != null && me && s.operator.fullName === me.innerText) {
                 operatorId = s.operator.id
                 afopername = s.operator.fullName
