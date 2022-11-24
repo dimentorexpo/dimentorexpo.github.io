c2dde51b4309bc275fb43b746d01824a1d36881f
grumstv <grumstv@gmail.com>
2022-11-24 15:35:18 +0300

    Update TestStatus.js

--- Modules/TestStatus.js
+++ Modules/TestStatus.js
@@ -54,7 +54,7 @@ async function operstatusleftbar() { // функция замены Script Packa
 						setTimeout( function() {
 							let massivvidapspiskaoperatorov = document.getElementById('operatorstp')
 							for (let k =0; k<massivvidapspiskaoperatorov.length; k++) {
-								if (massivvidapspiskaoperatorov.children[k+1].value == this.getAttribute('value')) {
+								if (massivvidapspiskaoperatorov.children[k+1].value == arofpers[i].value) {
 								massivvidapspiskaoperatorov.children[k+1].selected = true
 								findchatsoper()
 							}
