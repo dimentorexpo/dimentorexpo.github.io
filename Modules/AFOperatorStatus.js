cc0c5ad1add97e4f7eca8af51ed03776cb8284fc
grumstv <grumstv@gmail.com>
2022-11-25 07:01:03 +0300

    Update AFOperatorStatus.js

--- Modules/AFOperatorStatus.js
+++ Modules/AFOperatorStatus.js
@@ -106,10 +106,10 @@ async function operstatusleftbar() { // функция замены Script Packa
 	
 		peoplestatus.innerHTML = ''
 		
-		opstats.sort((prev, next) => {
-			if ( prev.status < next.operator.status ) return -1;
-			if ( prev.status < next.operator.status ) return 1;
-		});
+		// opstats.sort((prev, next) => {
+			// if ( prev.status < next.operator.status ) return -1;
+			// if ( prev.status < next.operator.status ) return 1;
+		// });
 		
 		if (opstats.length != 0) {
 			for (let i = 0; i < opstats.length; i++) {
