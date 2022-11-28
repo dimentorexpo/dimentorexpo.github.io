afd23613a421cc1b0f78722740b3cc89e1d546bb
grumstv <grumstv@gmail.com>
2022-11-28 23:58:12 +0300

    Update TaskTest.js

--- Modules/TaskTest.js
+++ Modules/TaskTest.js
@@ -127,10 +127,12 @@ taskBut.onclick = function () { // функция открытия окна дл
         document.getElementById('sendResponse').click()
 
         document.getElementById("responseTextarea1").addEventListener("DOMSubtreeModified", function () {
-            usersrv = JSON.parse(document.getElementById('responseTextarea1').getAttribute('getserviceinfonew'))
-			usersrvparsed = usersrv;
-			console.log(usersrvparsed)
-			document.getElementById('responseTextarea1').removeAttribute('getserviceinfonew')
+            usersrv =document.getElementById('responseTextarea1').getAttribute('getserviceinfonew')
+			if (usersrv != null) {
+				usersrvparsed = JSON.parse(usersrv)
+				console.log(usersrvparsed)
+				document.getElementById('responseTextarea1').removeAttribute('getserviceinfonew')
+			}
 		})
 		}
 	
