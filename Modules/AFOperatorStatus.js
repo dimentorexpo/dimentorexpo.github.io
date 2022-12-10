0a3e54e9b32ea4943b1c2953d953ee29cea7b780
grumstv <grumstv@gmail.com>
2022-12-10 10:18:53 +0300

    Update AFOperatorStatus.js

--- Modules/AFOperatorStatus.js
+++ Modules/AFOperatorStatus.js
@@ -94,7 +94,7 @@ async function operstatusleftbar() { // функция замены Script Packa
 				} else if (flagtpkc == 'SC' && result.onOperator[i].operator != null && result.onOperator[i].operator.fullName.match(/SC\D/)) {
 					opstats.push(result.onOperator[i])
 					for (let j=0; result.unAssigned[j] != undefined; j++) {
-						if (result.unAssigned[j].kb != '120181' && result.unAssigned[j].kb != '121300' && result.unAssigned[j].kb != '121438' && result.unAssigned[j].kb != '121446' && result.unAssigned[j].kb != '121832' && result.unAssigned[j].kb != '121430' && result.unAssigned[j].kb != '121879' && result.unAssigned[j].kb != '121777' && result.unAssigned[j].kb != '121447') {
+						if (result.unAssigned[j].kb == '121880' || result.unAssigned[j].kb == '121875' || result.unAssigned[j].kb == '121876' || result.unAssigned[j].kb != '121879' || result.unAssigned[j].kb == '121877' || result.unAssigned[j].kb == '121878' || result.unAssigned[j].kb == '121879' || result.unAssigned[j].kb != '121876' || result.unAssigned[j].kb == '121483' || result.unAssigned[j].kb == '121429' || result.unAssigned[j].kb == '121832' || result.unAssigned[j].kb == '121433' || result.unAssigned[j].kb == '121769' || result.unAssigned[j].kb == '121518') {
 							chatneraspcountleft = result.unAssigned[j].count
 						}
 					}	
@@ -133,7 +133,7 @@ async function operstatusleftbar() { // функция замены Script Packa
 					}	
 				} else if (flagtpkc == 'SC' && result.onOperator[i].operator != null && result.onOperator[i].operator.fullName.match(/SC\D/)) {
 					for (let j=0; result.unAssigned[j] != undefined; j++) {
-						if (result.unAssigned[j].kb != '120181' && result.unAssigned[j].kb != '121300' && result.unAssigned[j].kb != '121438' && result.unAssigned[j].kb != '121386' && result.unAssigned[j].kb != '121446' && result.unAssigned[j].kb != '121832' && result.unAssigned[j].kb != '121430' && result.unAssigned[j].kb != '121879' && result.unAssigned[j].kb != '121777' && result.unAssigned[j].kb != '121447') {
+						if (result.unAssigned[j].kb == '121880' || result.unAssigned[j].kb == '121875' || result.unAssigned[j].kb == '121876' || result.unAssigned[j].kb != '121879' || result.unAssigned[j].kb == '121877' || result.unAssigned[j].kb == '121878' || result.unAssigned[j].kb == '121879' || result.unAssigned[j].kb != '121876' || result.unAssigned[j].kb == '121483' || result.unAssigned[j].kb == '121429' || result.unAssigned[j].kb == '121832' || result.unAssigned[j].kb == '121433' || result.unAssigned[j].kb == '121769' || result.unAssigned[j].kb == '121518') {
 							chatneraspcountleft = result.unAssigned[j].count
 						}
 					}
