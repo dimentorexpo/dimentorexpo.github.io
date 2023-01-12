b2688a7cf6d204494ae0d47b48bc6180d35a152e
grumstv <grumstv@gmail.com>
2023-01-12 17:42:47 +0300

    Update CRMhelp-v1.js

--- CRMhelp-v1.js
+++ CRMhelp-v1.js
@@ -509,8 +509,8 @@ include("https://dimentorexpo.github.io/Lightbox/dist/js/lightbox.min.js") // п
 
 function checkforsoundplay() {
 	//document.body.click()
-	audioCRM.setAttribute('mute','true')
-	audioCRM.setAttribute('allow','autoplay')
+	audioCRM.muted = true
+	audioCRM.allow =autoplay
     if (localStorage.getItem('audioCRM') == 1 && window.location.href.indexOf('https://crm2.skyeng.ru/customer-support/start') !== -1) {
         if (document.getElementsByClassName('mat-button-disabled')[2] == undefined && document.getElementsByClassName('mat-button-wrapper')[13].innerText == 'Взять новую задачу') {    
             if (localStorage.getItem('repeatsound') == 0){
