ac25c7c9391e421f2172ad82dec31cd8f2fa4dc9
grumstv <grumstv@gmail.com>
2023-02-01 16:41:12 +0300

    Update Statistica1.js

--- Modules/Statistica1.js
+++ Modules/Statistica1.js
@@ -637,7 +637,7 @@ async function getopersSLA() {
 				for (let j=0; j<operdata.items.length;j++) {
 					await fetch("https://skyeng.autofaq.ai/api/conversations/" + operdata.items[j].conversationId).then(r=>r.json()).then(r=>fres=r)
 					if (fres.operatorId == activeopersId[i]) {
-						filteredarray.push({["operator"+[i+1]] : operdata.items[j].conversationId})
+						filteredarray.push({["operator"+[i+1]] : operdata.items[j].conversationId, ["duration"] : operdata.items[j].conversationDuration})
 					}
 				}
 			// arrayofSLA.push(operdata.total)
