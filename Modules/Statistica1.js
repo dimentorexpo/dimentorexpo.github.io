f6bae4161aa6df14a5a9342aed46125ee91cd68c
grumstv <grumstv@gmail.com>
2023-02-02 15:32:01 +0300

    Update Statistica1.js

--- Modules/Statistica1.js
+++ Modules/Statistica1.js
@@ -6,7 +6,7 @@ var win_StatisticaAF =  // описание формы чтобы не дава�
                                 <button title="скрывает меню" id="hidestatisticaaf" style="width:50px; background: #228B22;">hide</button>
 								<button id="clearstatawindow">🧹</button>
 								<input type="text" id="timeoutput" style="width:100px; text-align:center; background: blanchedalmond; font-weight: 700;" disabled></input>
-								<span style="width:450px"><div id="progress-bar" style="width: 0%; height: 20px; background: repeating-linear-gradient(-45deg, #877350, orange 5%, black 5%, black 10%);; text-align:center; font-weight:700; color:white;"></div></span>
+								<span style="width:450px"><div id="progress-bar" style="width: 0%; height: 20px; background-color: lightblue; text-align:center; font-weight:700; color:white;"></div></span>
 			    </span>
                         </div>
 						<div style="width: 750px; display:flex; justify-content: space-evenly; margin-bottom:5px;">
