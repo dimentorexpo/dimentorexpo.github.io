04f088aadcb2a2c041497db4269538e08cabf838
grumstv <grumstv@gmail.com>
2023-02-05 20:00:33 +0300

    Update Calendar.js

--- Modules/Calendar.js
+++ Modules/Calendar.js
@@ -109,9 +109,9 @@ document.getElementById('getCalendarData').onclick = function () {
 					tempor.setAttribute('style', 'width: 50%; height: 20px; color: bisque; font-weight:500; background:rgb(171 65 62); border:1px solid; font-size:12px; height:25px; margin-bottom:2px; text-align:center;');
 				} else if (availableslotsentries[i][1].CountEvent / availableslotsentries[i][1].CountSlot == 0) {
 					tempor.setAttribute('style', 'width: 50%; height: 20px; color: bisque; font-weight:500; background:rgb(62 158 83); border:1px solid; font-size:12px; height:25px; margin-bottom:2px; text-align:center;');
-				} else if (availableslotsentries[i][1].CountEvent / availableslotsentries[i][1].CountSlot > 0 && availableslotsentries[62][1].CountEvent / availableslotsentries[62][1].CountSlot <1) {
+				} else if (availableslotsentries[i][1].CountEvent / availableslotsentries[i][1].CountSlot > 0 && availableslotsentries[i][1].CountEvent / availableslotsentries[i][1].CountSlot <1) {
 					tempor.setAttribute('style', 'width: 50%; height: 20px; color: bisque; font-weight:500; background:rgb(196 161 55); color: black; border:1px solid; font-size:12px; height:25px; margin-bottom:2px; text-align:center;');
-				} else if (availableslotsentries[i][1].CountEvent == 0 && availableslotsentries[62][1].CountSlot == 0) {
+				} else if (availableslotsentries[i][1].CountEvent == 0 && availableslotsentries[i][1].CountSlot == 0) {
 					tempor.setAttribute('style', 'width: 50%; height: 20px; color: bisque; font-weight:500; background:rgb(113 121 125); color: black; border:1px solid; font-size:12px; height:25px; margin-bottom:2px; text-align:center;');
 				}
 
