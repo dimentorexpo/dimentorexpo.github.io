be1f6cd32c5b93251d8e67cfac013c38b6de6785
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-02-08 10:26:34 +0200

    кнопка Контроль

--- Modules/TaskCreate.js
+++ Modules/TaskCreate.js
@@ -389,6 +389,19 @@ document.getElementById('serviceinf').innerHTML = '';
             }
         }
 
+        document.getElementById('lowcontrol').onclick = function () {
+            document.getElementById('priority').children[1].selected = true;
+            document.getElementById('priority').style = "color:green;font-weight:600; width: 100%; height: 25px; text-align: center;"
+            document.getElementById('customerservice').children[8].selected = true;
+
+            for (i = 0; document.getElementsByClassName('expert-user_details-list')[1].childNodes[i] != undefined; i++) {
+                if (document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.innerText == "id")
+                    document.getElementById('taskuserid').value = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].innerText.split(' ')[0];
+            }
+
+            document.getElementById('taskcomment').value = document.getElementById('taskcomment').value + "\nКонтроль"
+        }
+
         document.getElementById('createtask').onclick = function () {
             let prioritystate;
             let csstate;
