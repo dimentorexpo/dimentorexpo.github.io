7482710605c432ad0057150e5570d2194a155a71
grumstv <grumstv@gmail.com>
2023-02-08 17:45:36 +0300

    Update Calendar.js

--- Modules/Calendar.js
+++ Modules/Calendar.js
@@ -21,6 +21,7 @@ var win_Calendar =  // описание формы чтобы не давала 
 						</div>
 						
 						<div id="slotList" style="display:none; margin-bottom: 5px; margin-left: 5px;">
+						<span id="chosenSlot"></span>
 						</div>
         </span>
 </div>`;
@@ -143,6 +144,7 @@ function getTimeSlots() {
 					document.getElementById('slotList').style.display = ""
 				
 				document.getElementById('slotList').innerHTML = ''
+				document.getElementById('chosenSlot').textContent = this.textContent
 				let testd = document.createElement('div')
 				testd.innerHTML = '<input style="width: 505px;">' + ' ' + '<button name="saveToCalend">💾</button>' + ' ' + '<button name="deleteFromCalend">❌</button>'
 				document.getElementById('slotList').appendChild(testd)
