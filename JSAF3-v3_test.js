e26ce17b37e9a411870165422752a42426c925b9
grumstv <grumstv@gmail.com>
2023-02-09 16:19:25 +0300

    fx

--- JSAF3-v3_test.js
+++ JSAF3-v3_test.js
@@ -657,13 +657,13 @@ function mystyles() {
 
 	@keyframes glowing-border {
 	  0% {
-		box-shadow: 0 0 10px blue;
+		box-shadow: 0 0 10px limegreen;
 	  }
 	  50% {
-		box-shadow: 0 0 20px blue;
+		box-shadow: 0 0 20px yellow;
 	  }
 	  100% {
-		box-shadow: 0 0 10px blue;
+		box-shadow: 0 0 10px orange;
 	  }
 	}
 
