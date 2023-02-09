e9fd44f48365ec08d534735c74223118d91603fb
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-02-09 17:54:40 +0200

    Update ThemesTest.js

--- Modules/ThemesTest.js
+++ Modules/ThemesTest.js
@@ -1,5 +1,5 @@
 var tableth;
-var btnthstyls = 'margin-left:2px; width:150px; height: 44px;';
+var btnthstyls = 'margin-left:2px; width:150px; height: 45px;';
 var btnTagstyles = 'margin-left:2px; width:125px; height: 25px;';
 var chbxTagstyles = 'margin: 2px; width: 20px;';
 
