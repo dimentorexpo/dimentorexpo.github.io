a152fde18cd44a14afb7b79fe6fc7351fea9bd21
grumstv <grumstv@gmail.com>
2023-02-17 08:04:56 +0300

    Update JSAF3-v3.js

--- JSAF3-v3.js
+++ JSAF3-v3.js
@@ -4364,6 +4364,7 @@ let butThemes = document.createElement('div')
 butThemes.id = "themes"
 butThemes.innerHTML = "Темы"
 butThemes.style = 'margin-right:15px; margin-top:15px; cursor:pointer;';
+butThemes.classList.add('ant-btn')
 
 let butJiraOpenForm = document.createElement('div')
 butJiraOpenForm.id = "JiraOpenForm"
