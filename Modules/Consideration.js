42c09242d169a0f5e02cac9c95e04f7b1261d63d
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-03-04 16:14:16 +0200

    Update Consideration.js

--- Modules/Consideration.js
+++ Modules/Consideration.js
@@ -1,23 +1,38 @@
-let infoconsideration  = document.createElement('p');
-infoconsideration.innerHTML = '<a style="color: black; width:40px; cursor: pointer;"> Info </a>';
-infoconsideration.style.display = 'none';
+var win_Infoconsid =  // описание элементов окна ссылок
+    `<span style="width: 800px">
+        <div style="width: 800px; font-size: 0.8rem;">
+            <div style="width: 49%; float: left;">
+                <p><b>Если передаешь компенсацию урока из-за серверных</b></p>
+                <p style="color:red"><b> •Добавь ссылку на disaster или ссылку на трэд<br>
+                •Напиши дату и время урока<br>
+                •Укажи какой статус урока выставлен</b></p>
+            </div>
+            <div style="width: 49%;float: right">
+                <p><b>Если делаешь запрос на компенсацию “повторное обращение по багу в мобильном приложении”</b></p> 
+                <p style="color:red"><b> Обязательно добавь ссылку на задачу в Jira </b></p>
+            </div>
+        </div>
+    </span>`;
 
+let wintInfoconsid = document.createElement('div'); // создание окна ссылок
+document.body.append(wintInfoconsid);
+wintInfoconsid.style.display = 'none';
+wintInfoconsid.innerHTML = win_Infoconsid;
 
 function startchecking(){
     if (document.URL == 'https://billing-marketing.skyeng.ru/accrual-operations/create') {
-        document.getElementById('selectedOperation').addEventListener("change", checkforaddinform)
+        document.getElementById('selectedOperation').addEventListener("change", addinformationform)
     }   
 }
 
-function checkforaddinform() {
+function addinformationform() {
     let TPcomp = document.getElementsByClassName('card-header')
     for (y = 0; y < TPcomp.length; y++) {
-        if (TPcomp[y].innerText == 'Компенсация за технические проблемы') { addinformationform()}
+        if (TPcomp[y].innerText == 'Компенсация за технические проблемы') {
+            formtoin = document.getElementsByClassName('ng-untouched ng-pristine ng-invalid')[0]
+            formtoin.insertBefore(wintInfoconsid)
+        }
     }
-}
-
-function addinformationform(){
-    console.log('Вижу')
 }
 
 startchecking()
