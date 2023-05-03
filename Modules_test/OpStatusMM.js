605efad10d901dd92a8e689fab79a12479206800
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-05-03 14:54:55 +0300

    Update OpStatusMM.js

--- Modules_test/OpStatusMM.js
+++ Modules_test/OpStatusMM.js
@@ -19,8 +19,9 @@ async function docheckopers() {
     else if (operdep == 'Prem')
         flagtpkc = 'Prem'
 
-    let currdate = getcurrentdate();
-    let currtime = getcurrenttime();
+    //let currdate = getcurrentdate();
+    //let currtime = getcurrenttime();
+    //let firtsmsg = `Статус операторов по состоянию на ${currdate} ${currtime}\\\\n`
 
 
 await fetch("https://skyeng.autofaq.ai/api/operators/statistic/currentState", {
@@ -38,7 +39,7 @@ await fetch("https://skyeng.autofaq.ai/api/operators/statistic/currentState", {
 
     let myString;
 if (opstats.length > 0) {
-     myString =`Статус операторов по состоянию на ${currdate} ${currtime}\\\\n| Чатов | Оператор | Статус |\\\\n|:---------:|:----------------------:|:----------:|\\\\n` + opstats.map(obj => `|${obj.aCnt} | ${obj.operator.fullName} | **[${obj.operator.status}]**|`).join('\\\\n') + `\\\\n\`\`\`Очередь ТП:\`\`\` ${chattpquecountleft}`;
+     myString =`| Чатов | Оператор | Статус |\\\\n|:---------:|:----------------------:|:----------:|\\\\n` + opstats.map(obj => `|${obj.aCnt} | ${obj.operator.fullName} | **[${obj.operator.status}]**|`).join('\\\\n') + `\\\\n\`\`\`Очередь ТП:\`\`\` ${chattpquecountleft}`;
 } else {
      myString =`На линии никого нет!\\\\n\`\`\`Очередь ТП:\`\`\` ${chattpquecountleft}`;
 }
