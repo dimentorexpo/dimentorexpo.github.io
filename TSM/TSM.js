e754aebf46102e2a24fb1e01bb92dbe64ed8eaa6
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-09-04 11:34:10 +0300

    ремонт

--- TSM/TSM.js
+++ TSM/TSM.js
@@ -1,4 +1,5 @@
 let allowedSites = ["vimbox.skyeng.ru", "new-teachers.skyeng.ru", "teachers.skyeng.ru", "student.skyeng.ru", "ttc.skyeng.ru"];
+let servicesites = ["skyeng.autofaq.ai/tickets/assigned","crm2.skyeng.ru"];
 let token;
 
 function loadmoduls(TSMScript){ // загрузка доп. модулей расширения
@@ -46,6 +47,22 @@ function firstLoad() { //первичаня загрузка страницы
 }
 
 if (allowedSites.includes(location.host)) { firstLoad() } // если нужная страница загружаем расширение
+
+if (servicesites.includes(location.host)){ // Если запущено CRM2 или AF
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message.action === "processSecondRequest") {
+            const Chatid = message.Chatid;
+            const messlink = 'https://mattermost.skyeng.tech/skyeng/pl/' + Chatid;
+            const SendMessage = 'Передано в канал #techsupport: ' + messlink;
+             if (location.host.includes('crm2.skyeng.ru')) {
+                copyToClipboardTSM(messlink);
+                alert(SendMessage);
+             } else if (location.host.includes('skyeng.autofaq.ai/tickets/assigned')) {
+                sendComment(SendMessage);
+             }
+        }
+    });
+}
 
 function checkelementt(a) { // проверка на какой элемент нажали
     let elem = document.elementFromPoint(a.clientX, a.clientY)
@@ -106,4 +123,51 @@ function fetchaddchat(userid1, userid2, method) { //вспомогательна
         "mode": "cors",
         "credentials": "include"
     });
+}
+
+async function sendComment(txt) { // функция отправки комментария
+    var values = await getInfo()
+    activeConvId = values[0]; uid = values[1];
+    if (activeConvId, uid) {
+        var txt2 = txt.split('\n').join('\\n')
+        var txt2 = txt2.split("\"").join("\\\"")
+        fetch("https://skyeng.autofaq.ai/api/reason8/answers", {
+            "headers": {
+                "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryH2CK1t5M3Dc3ziNW",
+            },
+            "body": "------WebKitFormBoundaryH2CK1t5M3Dc3ziNW\r\nContent-Disposition: form-data; name=\"payload\"\r\n\r\n{\"sessionId\":\"" + uid + "\",\"conversationId\":\"" + activeConvId + "\",\"text\":\"" + txt2 + "\",\"isComment\":true}\r\n------WebKitFormBoundaryH2CK1t5M3Dc3ziNW--\r\n",
+            "method": "POST",
+            "credentials": "include"
+        });
+    }
+}
+
+async function getInfo() { //функция получения инфо о чате и сервис айди
+    var sessionId = "";
+    var activeConvId = getChatId();
+    if (activeConvId){
+        await fetch("https://skyeng.autofaq.ai/api/conversations/" + activeConvId)
+			.then(r => r.json()).then(r => rdata = r)
+		sesid = rdata.sessionId;
+    } else { activeConvId = "" }
+
+    return [activeConvId, sessionId]
+}
+
+function getChatId() {
+    const hrefnow = window.location.href;
+    let chatId = '';
+    if (hrefnow.includes('tickets/assigned')) {
+        const iframeDocument = document.querySelector('[class^="NEW_FRONTEND"]').contentDocument || document.querySelector('[class^="NEW_FRONTEND"]').contentWindow.document;
+        const ConvArray = iframeDocument.querySelectorAll('#__next [class^="DialogsCard_Card"]');
+
+        for (let i = 0; i < ConvArray.length; i++) {
+            if (ConvArray[i].getAttribute('aria-selected') === 'true') {
+                chatId = ConvArray[i].getAttribute('data-conv-id');
+                break;
+            }
+        }
+    }
+
+    return chatId;
 }
