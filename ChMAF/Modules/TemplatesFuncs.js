83ebe23aa613fce5b5ed0a63e18e76c302ed01d9
dimentorexpo <85435517+dimentorexpo@users.noreply.github.com>
2023-11-03 08:39:33 +0200

    Update TemplatesFuncs.js

--- ChMAF/Modules/TemplatesFuncs.js
+++ ChMAF/Modules/TemplatesFuncs.js
@@ -759,7 +759,7 @@ async function buttonsFromDoc(butName) { // функция отправки ша
         return
     }
 
-    if (butName == '🖕Отказ' && document.getElementById('AF_Refuseformnew').style.display == 'none') // если кнопка отказ открывает форму отказа и если повторно нажали не закрываем форму
+    if (butName == '❌Отказ' && document.getElementById('AF_Refuseformnew').style.display == 'none') // если кнопка отказ открывает форму отказа и если повторно нажали не закрываем форму
         document.getElementById('otkaz').click();
 
     if (((butName == '🤬Негатив ОС') || (butName == '🖼Нет изобр в ДЗ ЛК') || (butName == '💨Сброс ответов ДЗ ЛК') || (butName == '🔇Звук ответов ЛК') || (butName == '🖥Размер видео') || butName == ('🖼📱Нет изобр ДЗ в МП')) && document.getElementById('AF_Smartroomform').style.display == 'none')
